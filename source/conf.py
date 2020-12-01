3b4e0819f62bab705f77ce6334a0466121f4b384
dorme <d.orme@imperial.ac.uk>
2020-12-01 21:18:15 +0000

    Switch sphinx to RTD theme

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,9 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.autosummary',
               'sphinxcontrib.bibtex',
-              'recommonmark']
+              'sphinx_rtd_theme',
+              'recommonmark'
+              ]
 
 autodoc_default_flags = ['members']
 autosummary_generate = True
@@ -57,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
