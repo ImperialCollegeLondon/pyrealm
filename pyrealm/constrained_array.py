f235dfa0325cea0ea3d4c920f1f25dbeaeea47fd
dorme <d.orme@imperial.ac.uk>
2021-07-21 11:28:29 +0100

    Extended ConstrainedArray and constraint_factory to make constraint factories also check for already constrained inputs

--- pyrealm/constrained_array.py
+++ pyrealm/constrained_array.py
@@ -1,5 +1,19 @@
 import numpy as np
 import warnings
+from typing import Union, Callable
+from numbers import Number
+
+# TODO - include interval type as '[]', '()', '[)', '[i', etc.
+#        using that str to set the bounding ufunc
+
+# TODO - at the moment, functions using ConstrainedArray only check
+#        that they are getting a CA, not that it has the _right_ constraints
+#        Being paranoid, a user could create CAs and pass them in incorrectly
+#        by getting the argument order wrong.
+#        Maybe add an id attribute and also a check_constraint(arr, factory)
+#        helper function.
+#        This can still be defeated but a user would have to deliberately set
+#        an incorrect ID.
 
 
 class ConstrainedArray(np.ma.core.MaskedArray):
@@ -16,6 +30,7 @@ class ConstrainedArray(np.ma.core.MaskedArray):
         input_array: An np.ndarray object
         lower: The value of the lower constraint
         upper: The value of the upper constraint
+        label: A string giving a descriptive label of the constrained contents
 
     Returns:
 
@@ -33,7 +48,10 @@ class ConstrainedArray(np.ma.core.MaskedArray):
         50
     """
 
-    def __new__(cls, input_array: np.ndarray, lower=-np.infty, upper=np.infty):
+    def __new__(cls, input_array: np.ndarray,
+                lower: Number = -np.infty,
+                upper: Number = np.infty,
+                label: str = None):
 
         # Input array is an already formed ndarray instance
         # We first cast to be our class type
@@ -49,21 +67,28 @@ class ConstrainedArray(np.ma.core.MaskedArray):
         obj = np.ma.masked_outside(obj, v1=lower, v2=upper)
 
         # Record some details of the constraint
-        obj.constrained = True
-        obj.constraints = [lower, upper]
+        obj.limits = [lower, upper]
         obj.n_constrained = obj.mask.sum() - n_masked
+        obj.label = label
 
         return obj
 
 
-def constraint_factory(label: str, lower=-np.infty, upper=np.infty):
-    r"""This is a function factory used to create ConstrainedArrays
-    with different limits. The generated functions issue a warning when the
-    constraints are enforced using the provided label to aid reporting.
+def constraint_factory(label: str,
+                       lower: Number = -np.infty,
+                       upper: Number = np.infty):
+    r"""This is a function factory used to create functions to generate
+    ConstrainedArrays with preset limits and labels. The generated functions
+    issue a warning when the constraints are enforced using the provided label
+    to aid reporting.
+
+    The resulting function also acts a validator. If the input is already
+    a ConstrainedArray, the label and limits are compared to check that the
+
 
     Parameters:
 
-        label: A string to be used when reporting constraints being enforced.
+        label: A string giving a descriptive label of the constrained contents
         lower: The lower constraint to enforce
         upper: The upper constraint to enforce
 
@@ -76,11 +101,22 @@ def constraint_factory(label: str, lower=-np.infty, upper=np.infty):
         >>> vals_c = temp_constraint(vals)
         >>> vals_c.sum()
         50
-    """
+        >>> diff = ConstrainedArray(vals, -50, 120, 'foo')
+        >>> diff = temp_constraint(diff)
+        Traceback (most recent call last):
+        ...
+        RuntimeError: Existing input constraints do not match checking constraints
+        """
 
     def action(arr: np.ndarray):
 
-        arr = ConstrainedArray(arr, lower=lower, upper=upper)
+        if isinstance(arr, ConstrainedArray):
+            if arr.limits != [lower, upper] or arr.label != label:
+                raise RuntimeError('Existing input constraints do not match checking constraints')
+            else:
+                return arr
+
+        arr = ConstrainedArray(arr, lower=lower, upper=upper, label=label)
 
         if arr.n_constrained:
             warnings.warn(f'{arr.n_constrained} {label} values outside '
@@ -88,4 +124,13 @@ def constraint_factory(label: str, lower=-np.infty, upper=np.infty):
                           category=RuntimeWarning)
         return arr
 
+    # Annotate the function with the limits and label to facilitate checking.
+    action.limits = [lower, upper]
+    action.label = label
+
+    # Add a docstring
+    action.__doc__ = "Test"
+
     return action
+
+
