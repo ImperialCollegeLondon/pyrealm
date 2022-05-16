4628fbaaae708af74f366bf78dabc60709efcba6
dorme <d.orme@imperial.ac.uk>
2022-05-16 12:13:17 +0100

    doctest on calc_density_h2o: avoiding OS specific precision from CI testing. HACK

--- pyrealm/pmodel.py
+++ pyrealm/pmodel.py
@@ -3,6 +3,7 @@ from typing import Optional, Union
 from warnings import warn
 
 import numpy as np
+import bottleneck as bn
 
 from pyrealm.bounds_checker import bounds_checker
 from pyrealm.param_classes import PModelParams
@@ -39,8 +40,8 @@ def calc_density_h2o(tc: Union[float, np.ndarray],
 
     Examples:
 
-        >>> round(calc_density_h2o(20, 101325), 4)
-        998.2056
+        >>> round(calc_density_h2o(20, 101325), 3)
+        998.206
     """
 
     # DESIGN NOTE:
@@ -1718,3 +1719,77 @@ class JmaxLimitation:
         self.f_v = 1.0
         self.f_j = 1.0
 
+
+
+
+
+
+
+# subdaily Pmodel
+
+
+def memory_effect(values: np.ndarray, alpha: float = 0.067):
+    r"""
+    Vcmax and Jmax do not converge instantaneously to acclimated optimal
+    values. This function estimates how the actual Vcmax and Jmax track 
+    a time series of calculated optimal values assuming instant acclimation.
+
+    The estimation uses the paramater `alpha` (:math:`\alpha`) to control
+    the speed of convergence of the estimated values (:math:`E`) to the 
+    calculated optimal values (:math:`O`):
+
+    ::math
+
+        E_{t} = E_{t-1}(1 - \alpha) + O_{t} \alpha 
+
+    For :math:`t_{0}`, the first value in the optimal values is used so
+    :math:`E_{0} = O_{0}`.
+    """
+
+
+    # TODO - NA handling
+    # TODO - think about filters here - I'm sure this is a filter which
+    #        could generalise to longer memory windows.
+    # TODO - need a version that handles time slices for low memory looping
+    #        over arrays.
+
+    memory_values = np.empty_like(values, dtype=np.float)
+    memory_values[0] = values[0]
+
+    for idx in range(1, len(memory_values)):
+        memory_values[idx] = memory_values[idx - 1] * (1 - alpha) + values[idx] * alpha
+
+    return memory_values
+
+
+
+def interpolate_rates_forward(tk: np.ndarray, ha: float, 
+                              values: np.ndarray, values_idx: np.ndarray) -> np.ndarray:
+    """
+    This is a specialised interpolation function used for Jmax and Vcmax. Given
+    a time series of temperatures in Kelvin (`tk`) and a set of Jmax25 or
+    Vcmax25 values observed at indices (`values_idx`) along that time series,
+    this pushes those values along the time series and then rescales to the
+    observed temperatures.
+
+    The effect is that the plant 'sets' its response at a given point of the day
+    and then maintains that same behaviour until a similar reference time the
+    following day.
+
+    Note that the beginning of the sequence will be filled with np.nan values
+    unless values_idx[0] = 0.
+
+    Arguments:
+        tk: A time series of temperature values (Kelvin)
+        ha: An Arrhenius constant.
+        values: An array of rates at standard temperature predicted at points along tk.
+        values_idx: The indices of tk at which values are predicted.
+    """
+    
+    v = np.empty_like(tk)
+    v[:] = np.nan
+
+    v[values_idx] = values
+    v = bn.push(v)
+
+    return v * calc_ftemp_arrh(tk=tk, ha=ha)
