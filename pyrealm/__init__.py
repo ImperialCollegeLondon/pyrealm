f27659066d789c7ed8122b3674ca0decbefa5eac
dorme <d.orme@imperial.ac.uk>
2022-11-29 09:57:38 +0000

    updated mypy settings, mypy fixes on __init__

--- pyrealm/__init__.py
+++ pyrealm/__init__.py
@@ -5,12 +5,7 @@ TODO: complete this documentation
 
 import os
 import warnings
-
-# _ROOT = os.path.abspath(os.path.dirname(__file__))
-#
-#
-# def get_data_path(path):
-#     return os.path.join(_ROOT, 'data', path)
+from typing import Optional, Type, Union
 
 
 class ExperimentalFeatureWarning(Warning):
@@ -20,7 +15,7 @@ class ExperimentalFeatureWarning(Warning):
     experimental features in the code.
     """
 
-    def __init__(self, message) -> None:
+    def __init__(self, message: str) -> None:
         self.message = message
 
     def __str__(self) -> str:
@@ -29,24 +24,15 @@ class ExperimentalFeatureWarning(Warning):
 
 # Setup warnings to simpler one line warning
 def warning_on_one_line(
-    message: str, category: str, filename: str, lineno: int, file=None, line=None
-):
+    message: Union[Warning, str],
+    category: Type[Warning],
+    filename: str,
+    lineno: int,
+    line: Optional[str] = None,
+) -> str:
     """Provide a simple one line warning for use in docstrings."""
     filename = os.path.join("pyrealm", os.path.basename(filename))
     return "%s:%s: %s: %s\n" % (filename, lineno, category.__name__, message)
 
 
 warnings.formatwarning = warning_on_one_line
-
-# # And provide a decorator to catch warnings in doctests
-# # https://stackoverflow.com/questions/2418570/
-
-# def stderr_to_stdout(func):
-#     def wrapper(*args):
-#         stderr_bak = sys.stderr
-#         sys.stderr = sys.stdout
-#         try:
-#             return func(*args)
-#         finally:
-#             sys.stderr = stderr_bak
-#     return wrapper
