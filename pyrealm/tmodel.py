a3f13f0e6d74e7ca89e3972ad40ff56d91a64eae
dorme <d.orme@imperial.ac.uk>
2022-12-09 21:37:05 +0000

    Mypy checking on tmodel and substantially improved test suite. grow_ttree temporarily redacted - upcoming changes

--- pyrealm/tmodel.py
+++ pyrealm/tmodel.py
@@ -13,12 +13,11 @@ an estimate of gross primary productivity (GPP).
   time given a time series of GPP.
 """
 
-from typing import Tuple, Union
+from typing import Union
 
 import numpy as np
 
 from pyrealm.param_classes import TModelTraits
-from pyrealm.utilities import check_input_shapes
 
 # Design Notes:
 #
@@ -219,33 +218,33 @@ class TTree:
 
         # Height of tree from diameter, Equation (4) of Li ea.
         self._height = self.traits.h_max * (
-            1 - np.exp(-self.traits.a_hd * self.diameter / self.traits.h_max)
+            1 - np.exp(-self.traits.a_hd * self._diameter / self.traits.h_max)
         )
 
         # Crown area of tree, Equation (8) of Li ea.
         self._crown_area = (
             ((np.pi * self.traits.ca_ratio) / (4 * self.traits.a_hd))
-            * self.diameter
-            * self.height
+            * self._diameter
+            * self._height
         )
 
         # Crown fraction, Equation (11) of Li ea.
-        self._crown_fraction = self.height / (self.traits.a_hd * self.diameter)
+        self._crown_fraction = self._height / (self.traits.a_hd * self._diameter)
 
         # Masses
         self._mass_stm = (
-            (np.pi / 8) * (self.diameter**2) * self.height * self.traits.rho_s
+            (np.pi / 8) * (self._diameter**2) * self._height * self.traits.rho_s
         )
-        self._mass_fol = self.crown_area * self.traits.lai * (1 / self.traits.sla)
+        self._mass_fol = self._crown_area * self.traits.lai * (1 / self.traits.sla)
         self._mass_swd = (
-            self.crown_area
+            self._crown_area
             * self.traits.rho_s
-            * self.height
-            * (1 - self.crown_fraction / 2)
+            * self._height
+            * (1 - self._crown_fraction / 2)
             / self.traits.ca_ratio
         )
 
-        # Clear any calculated growth values
+        # Flag any calculated growth values as outdated
         self.growth_calculated = False
 
     def calculate_growth(self, gpp: Union[float, np.ndarray]) -> None:
@@ -261,27 +260,27 @@ class TTree:
 
         # GPP fixed per m2 of crown
         self._gpp_raw = gpp
-        gpp_unit_cr = self.gpp_raw * (
+        gpp_unit_cr = self._gpp_raw * (
             1 - np.exp(-(self.traits.par_ext * self.traits.lai))
         )
-        self._gpp_actual = self.crown_area * gpp_unit_cr
+        self._gpp_actual = self._crown_area * gpp_unit_cr
 
         # Respiration costs (Eqn 13 of Li ea)
         # - sapwood, fine root and foliage maintenance
-        self._resp_swd = self.mass_swd * self.traits.resp_s
+        self._resp_swd = self._mass_swd * self.traits.resp_s
         self._resp_frt = (
-            self.traits.zeta * self.traits.sla * self.mass_fol * self.traits.resp_r
+            self.traits.zeta * self.traits.sla * self._mass_fol * self.traits.resp_r
         )
-        self._resp_fol = self.gpp_actual * self.traits.resp_f
+        self._resp_fol = self._gpp_actual * self.traits.resp_f
 
         # Net primary productivity
         self._npp = self.traits.yld * (
-            self.gpp_actual - self.resp_fol - self.resp_frt - self._resp_swd
+            self._gpp_actual - self._resp_fol - self._resp_frt - self._resp_swd
         )
 
         # Turnover costs for foliage and fine roots
         self._turnover = (
-            self.crown_area
+            self._crown_area
             * self.traits.lai
             * (
                 (1 / (self.traits.sla * self.traits.tau_f))
@@ -295,12 +294,12 @@ class TTree:
             np.pi
             / 8
             * self.traits.rho_s
-            * self.diameter
+            * self._diameter
             * (
                 self.traits.a_hd
-                * self.diameter
-                * (1 - (self.height / self.traits.h_max))
-                + 2 * self.height
+                * self._diameter
+                * (1 - (self._height / self.traits.h_max))
+                + 2 * self._height
             )
         )
 
@@ -308,105 +307,111 @@ class TTree:
             self.traits.lai
             * ((np.pi * self.traits.ca_ratio) / (4 * self.traits.a_hd))
             * (
-                self.traits.a_hd * self.diameter * (1 - self.height / self.traits.h_max)
-                + self.height
+                self.traits.a_hd
+                * self._diameter
+                * (1 - self._height / self.traits.h_max)
+                + self._height
             )
             * (1 / self.traits.sla + self.traits.zeta)
         )
 
         # Actual increments
-        self._delta_d = (self.npp - self.turnover) / (self.d_mass_s + self.d_mass_fr)
-        self._delta_mass_stm = self.d_mass_s * self.delta_d
-        self._delta_mass_frt = self.d_mass_fr * self.delta_d
+        self._delta_d = (self._npp - self._turnover) / (
+            self._d_mass_s + self._d_mass_fr
+        )
+        self._delta_mass_stm = self._d_mass_s * self._delta_d
+        self._delta_mass_frt = self._d_mass_fr * self._delta_d
 
         self.growth_calculated = True
 
 
-def grow_ttree(
-    gpp: Union[float, np.ndarray],
-    d_init: Union[float, np.ndarray],
-    time_axis: int,
-    traits: TModelTraits = TModelTraits(),
-    outvars: Tuple[str, ...] = ("diameter", "height", "crown_area", "delta_d"),
-) -> dict:
-    """Fit a growth time series using the T Model.
+# def grow_ttree(
+#     gpp: Union[float, np.ndarray],
+#     d_init: Union[float, np.ndarray],
+#     time_axis: int,
+#     traits: TModelTraits = TModelTraits(),
+#     outvars: Tuple[str, ...] = ("diameter", "height", "crown_area", "delta_d"),
+# ) -> dict:
+#     """Fit a growth time series using the T Model.
 
-    This function fits the T Model incrementally to a set of modelled plants,
-    given a time series of GPP estimates.
+#     This function fits the T Model incrementally to a set of modelled plants,
+#     given a time series of GPP estimates.
 
-    Args:
-        gpp: An array of GPP values
-        d_init: An array of starting diameters
-        traits: Traits to be used
-        time_axis: An axis in P0 and d that represents an annual time series
-        outvars: A list of Tree properties to store.
+#     Args:
+#         gpp: An array of GPP values
+#         d_init: An array of starting diameters
+#         traits: Traits to be used
+#         time_axis: An axis in P0 and d that represents an annual time series
+#         outvars: A list of Tree properties to store.
 
-    Returns:
-        A dictionary of estimates for the time series, exporting the values
-        specified in `outvars`
-    """
+#     Returns:
+#         A dictionary of estimates for the time series, exporting the values
+#         specified in `outvars`
+#     """
 
-    # The gpp array should contain a time axis to increment over. The d_init input
-    # should then be the same shape as P0 _without_ that time axis dimension
-    # and the loop will iterate over the years
+#     # The gpp array should contain a time axis to increment over. The d_init input
+#     # should then be the same shape as P0 _without_ that time axis dimension
+#     # and the loop will iterate over the years
 
-    gpp_shape = gpp.shape
-    if time_axis < 0 or time_axis >= len(gpp_shape):
-        raise RuntimeError(f"time_axis must be >= 0 and <= {len(gpp_shape) - 1}")
+#     gpp_shape = gpp.shape
+#     if time_axis < 0 or time_axis >= len(gpp_shape):
+#         raise RuntimeError(f"time_axis must be >= 0 and <= {len(gpp_shape) - 1}")
 
-    # Check that the input shapes for a single year match the shape of the
-    # initial diameters
-    single_year_gpp = np.take(gpp, indices=0, axis=time_axis)
-    _ = check_input_shapes(single_year_gpp, d_init)
+#     # Check that the input shapes for a single year match the shape of the
+#     # initial diameters
+#     single_year_gpp = np.take(gpp, indices=0, axis=time_axis)
+#     _ = check_input_shapes(single_year_gpp, d_init)
 
-    # TODO: - handle 1D GPP time series applied to more than one diameter
+#     # TODO: - handle 1D GPP time series applied to more than one diameter
 
-    # Initialise the Tree object
-    tree = TTree(traits, d_init)
+#     # Initialise the Tree object
+#     tree = TTree(traits, d_init)
 
-    # Check the requested outvars
-    if "diameter" not in outvars:
-        raise RuntimeWarning("output_vars must include diameter")
+#     # Check the requested outvars
+#     if "diameter" not in outvars:
+#         raise RuntimeWarning("output_vars must include diameter")
 
-    badvars = []
-    for var in outvars:
+#     badvars = []
+#     for var in outvars:
 
-        try:
-            _ = getattr(tree, var)
-        except AttributeError:
-            badvars.append(var)
+#         try:
+#             _ = getattr(tree, var)
+#         except AttributeError:
+#             badvars.append(var)
 
-    if badvars:
-        raise RuntimeError(f"Unknown tree properties in outvars: {', '.join(badvars)}")
+#     if badvars:
+#         raise RuntimeError(
+#           f"Unknown tree properties in outvars: {', '.join(badvars)}"
+#           )
 
-    # Create an array to store the requested variables by adding a dimension
-    # to the gpp input with length set to the number of variables
-    output_shape = gpp_shape + tuple([len(outvars)])
-    output = np.zeros(output_shape)
+#     # Create an array to store the requested variables by adding a dimension
+#     # to the gpp input with length set to the number of variables
+#     output_shape = gpp_shape + tuple([len(outvars)])
+#     output = np.zeros(output_shape)
 
-    # Create an indexing object to insert values into the output. This is
-    # a bit obscure: the inputs have a time axis and an arbitrary number
-    # of other dimensions and the output adds another dimension for the
-    # output variables. So this object creates a slice (:) on _all_ dimensions
-    # and the loop then replaces the time axis and variable axis with integers.
-    output_index = [slice(None)] * output.ndim
+#     # Create an indexing object to insert values into the output. This is
+#     # a bit obscure: the inputs have a time axis and an arbitrary number
+#     # of other dimensions and the output adds another dimension for the
+#     # output variables. So this object creates a slice (:) on _all_ dimensions
+#     # and the loop then replaces the time axis and variable axis with integers.
+#     output_index = [slice(None)] * output.ndim
 
-    # Loop over the gpp time axis
-    for year in np.arange(gpp_shape[time_axis]):
+#     # Loop over the gpp time axis
+#     for year in np.arange(gpp_shape[time_axis]):
 
-        # Calculate the growth based on the current year of gpp
-        tree.calculate_growth(np.take(gpp, indices=year, axis=time_axis))
+#         # Calculate the growth based on the current year of gpp
+#         tree.calculate_growth(np.take(gpp, indices=year, axis=time_axis))
 
-        # Store the requested variables into the output array
-        for var_idx, each_var in enumerate(outvars):
+#         # Store the requested variables into the output array
+#         for var_idx, each_var in enumerate(outvars):
 
-            # Extract variable values from tree into output - set the last index
-            # (variable dimension) to the variable index and the time axis to the year
-            output_index[-1] = var_idx
-            output_index[time_axis] = year
-            output[tuple(output_index)] = getattr(tree, each_var)
+#             # Extract variable values from tree into output - set the last index
+#             # (variable dimension) to the variable index and the time axis to the year
+#             output_index[-1] = var_idx
+#             output_index[time_axis] = year
+#             output[tuple(output_index)] = getattr(tree, each_var)
 
-        # Now update the tree object
-        tree.set_diameter(tree.diameter + getattr(tree, "delta_d"))
+#         # Now update the tree object
+#         tree.set_diameter(tree.diameter + getattr(tree, "delta_d"))
 
-    return output
+#     return output
--- tests/tmodel/test_tmodel.py
+++ tests/tmodel/test_tmodel.py
@@ -5,162 +5,245 @@ Tests the init, grow_ttree and other methods of TModel
 
 import csv
 import os
-from contextlib import contextmanager
+from contextlib import nullcontext as does_not_raise
 
 import numpy as np
 import pytest
 
-from pyrealm import param_classes, tmodel
-
-# flake8: noqa D103 - docstrings on unit tests
-
-# ------------------------------------------
-# Null context manager to include exception testing in test paramaterisation
-# ------------------------------------------
-
-RPMODEL_C4_BUG = True
-
-
-@contextmanager
-def does_not_raise():
-    yield
-
-
-# ------------------------------------------
-# Fixtures: inputs and expected values
-# ------------------------------------------
+# Fixtures: inputs and expected values from the original implementation in R
 
 
 @pytest.fixture(scope="module")
 def rvalues():
-    """Fixture to load test inputs from file."""
+    """Fixture to load test inputs from file.
+
+    This is a time series of growth using the default trait values in R, mapped to the
+    internal property names used in TTree
+    """
+    from pyrealm.tmodel import TModelTraits
 
     test_dir = os.path.dirname(os.path.abspath(__file__))
+
     with open(os.path.join(test_dir, "rtmodel_output.csv")) as infile:
         rdr = csv.DictReader(infile, quoting=csv.QUOTE_NONNUMERIC)
         values = [v for v in rdr]
 
+    name_map = (
+        ("dD", "delta_d"),
+        ("D", "diameter"),
+        ("H", "height"),
+        ("Ac", "crown_area"),
+        ("Wf", "mass_fol"),
+        ("Ws", "mass_stm"),
+        ("Wss", "mass_swd"),
+        ("GPP", "gpp_actual"),
+        ("Rm1", "resp_swd"),
+        ("Rm2", "resp_frt"),
+        ("dWs", "delta_mass_stm"),
+        ("dWfr", "delta_mass_frt"),
+    )
+
+    # copy values under R names to py names
+    traits = TModelTraits()
+
+    for row in values:
+        for rnm, pynm in name_map:
+
+            # Fix some scaling differences:
+            if pynm == "delta_d":
+                # The R tmodel implementation rescales reported delta_d as
+                # a radial increase in millimetres, not diameter increase in metres
+                row[pynm] = row[rnm] / 500
+            elif pynm == "gpp_actual":
+                # The R tmodel implementation slices off foliar respiration costs from
+                # GPP before doing anything - the pyrealm.tmodel implementation keeps
+                # this cost within the tree calculation
+                row[pynm] = row[rnm] / (1 - traits.resp_f)
+            else:
+                row[pynm] = row[rnm]
+
     return values
 
 
-@pytest.fixture(scope="module")
-def pyvalues():
-    """Run the same time series in python"""
+@pytest.mark.parametrize(argnames="row", argvalues=np.arange(0, 100, 10))
+def test_tmodel_init(rvalues, row):
+    """Test the geometry calculation of __init__ using the exemplar R data."""
 
-    # Do the same simulation as the R implementation - a single stem of
-    # diameter 0.1 exposed to 100 years of GPP of 7.
-    traits = param_classes.TModelTraits()
-    py_vars = (
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    ttree = TTree(traits=TModelTraits, diameters=rvalues[row]["diameter"])
+
+    for geom_est in ("height", "crown_area", "mass_fol", "mass_stm", "mass_swd"):
+        assert np.allclose(getattr(ttree, geom_est), rvalues[row][geom_est])
+
+
+@pytest.mark.parametrize(argnames="row", argvalues=np.arange(0, 100, 10))
+def test_tmodel_set_diameter(rvalues, row):
+    """Test the geometry calculation using set_diameter using the exemplar R data."""
+
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    ttree = TTree(traits=TModelTraits, diameters=0.001)
+    ttree.set_diameter(rvalues[row]["diameter"])
+
+    for geom_est in ("height", "crown_area", "mass_fol", "mass_stm", "mass_swd"):
+        assert np.allclose(getattr(ttree, geom_est), rvalues[row][geom_est])
+
+
+def test_tmodel_init_array(rvalues):
+    """Test the geometry calculation using an __init__array using the exemplar R data."""
+
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    diams = np.array([rw["diameter"] for rw in rvalues])
+    ttree = TTree(traits=TModelTraits, diameters=diams)
+
+    for geom_est in ("height", "crown_area", "mass_fol", "mass_stm", "mass_swd"):
+        vals = [rw[geom_est] for rw in rvalues]
+        assert np.allclose(getattr(ttree, geom_est), vals)
+
+
+@pytest.mark.parametrize(
+    argnames="sequence, raises",
+    argvalues=[
+        ("init_only", pytest.raises(RuntimeError)),
+        ("init_grow", does_not_raise()),
+        ("init_set", pytest.raises(RuntimeError)),
+        ("init_grow_set", pytest.raises(RuntimeError)),
+        ("init_grow_set_grow", does_not_raise()),
+    ],
+)
+def test_tmodel_growth_access(sequence, raises):
+    """Check masking of growth estimates
+
+    Tests that the accessors for growth properties are properly masked when not set
+    or outdated.
+    """
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    ttree = TTree(traits=TModelTraits(), diameters=0.1)
+
+    if sequence == "init_only":
+        pass
+    elif sequence == "init_grow":
+        ttree.calculate_growth(7)
+    elif sequence == "init_set":
+        ttree.set_diameter(0.2)
+    elif sequence == "init_grow_set":
+        ttree.calculate_growth(7)
+        ttree.set_diameter(0.2)
+    elif sequence == "init_grow_set_grow":
+        ttree.calculate_growth(7)
+        ttree.set_diameter(0.2)
+        ttree.calculate_growth(7)
+
+    with raises:
+        val = ttree.delta_d
+
+
+@pytest.mark.parametrize(argnames="row", argvalues=np.arange(0, 100, 10))
+def test_tmodel_calculate_growth(rvalues, row):
+    """Test calculate_growth with scalars
+
+    Runs a test of the tmodel.TTree against output from the R implementation.
+    The values in the test come from simulating a 100 year run starting from
+    a stem diameter of 0.1 and with an annual GPP value of 7. Each row in the
+    file is the successive growth, but this test just runs some values from the
+    sequence.
+    """
+
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    # create a tree with the initial diameter given in the row
+    ttree = TTree(TModelTraits(), diameters=rvalues[row]["diameter"])
+    ttree.calculate_growth(7)
+
+    for growth_est in (
         "delta_d",
-        "diameter",
-        "height",
-        "crown_area",
-        "mass_fol",
-        "mass_stm",
-        "mass_swd",
         "gpp_actual",
         "resp_swd",
         "resp_frt",
         "delta_mass_stm",
         "delta_mass_frt",
-    )
-    values = tmodel.grow_ttree(
-        np.array([[7] * 100]), np.array([0.1]), 1, traits=traits, outvars=py_vars
-    )
-
-    return traits, values
+    ):
+        assert np.allclose(getattr(ttree, growth_est), rvalues[row][growth_est])
 
 
-# ------------------------------------------
-# Tests
-# ------------------------------------------
+def test_tmodel_calculate_growth_array(rvalues):
+    """Test calculate_growth with an array."""
+
+    from pyrealm.param_classes import TModelTraits
+    from pyrealm.tmodel import TTree
+
+    # create a tree with the initial diameter given in the row
+    diams = np.array([rw["diameter"] for rw in rvalues])
+    ttree = TTree(traits=TModelTraits, diameters=diams)
+    ttree.calculate_growth(7)
+
+    for growth_est in (
+        "delta_d",
+        "gpp_actual",
+        "resp_swd",
+        "resp_frt",
+        "delta_mass_stm",
+        "delta_mass_frt",
+    ):
+        vals = [rw[growth_est] for rw in rvalues]
+        assert np.allclose(getattr(ttree, growth_est), vals)
 
 
-def test_ttree(rvalues):
-    """Runs a test of the tmodel.TTree against output from an R implementation.
-    The values in the test come from simulating a 100 year run starting from
-    a stem diameter of 0.1 and with an annual GPP value of 7. Each row in the
-    file is the successive growth, but this test just runs each value in
-    sequence.
+# @pytest.mark.parametrize(
+#     "varname",
+#     [
+#         (0, ("dD", "delta_d")),
+#         (1, ("D", "diameter")),
+#         (2, ("H", "height")),
+#         (3, ("Ac", "crown_area")),
+#         (4, ("Wf", "mass_fol")),
+#         (5, ("Ws", "mass_stm")),
+#         (6, ("Wss", "mass_swd")),
+#         (7, ("GPP", "gpp_actual")),
+#         (8, ("Rm1", "resp_swd")),
+#         (9, ("Rm2", "resp_frt")),
+#         (10, ("dWs", "delta_mass_stm")),
+#         (11, ("dWfr", "delta_mass_frt")),
+#     ],
+# )
+# def test_grow_ttree(rvalues, pyvalues, varname):
+#     """Runs a test of the tmodel.grow_ttree against the same R output.
+#     In this case, the iteration process through time is tested. If the
+#     previous test is successful then the values being fed forward in the
+#     iteration _should_ be all fine, but this checks that the time iteration
+#     is being run correctly in addition to the implementation of the model being
+#     correct
 
-    Args:
-        values: Expected outputs from R implementation
+#     Args:
+#         values: Expected outputs from R implementation
 
-    """
+#     """
 
-    for row in rvalues:
+#     idx, (rv, pyv) = varname
+#     traits, pyvalues = pyvalues
 
-        # create a tree with the initial diameter given in the row
-        tree = tmodel.TTree()
-        tree.set_diameter(row["D"])
-        tree.calculate_growth(7)
+#     # Get all the R values across timesteps into an array
+#     r_var = np.array([rw[rv] for rw in rvalues])
+#     # Get the matching py output
+#     py_var = pyvalues[..., idx]
 
-        # The R tmodel implementation rescales reported delta_d as
-        # a radial increase in millimetres.
-        assert np.allclose(tree.delta_d * 500, row["dD"])
-        assert np.allclose(tree.diameter, row["D"])
-        assert np.allclose(tree.height, row["H"])
-        assert np.allclose(tree.crown_area, row["Ac"])
-        assert np.allclose(tree.mass_fol, row["Wf"])
-        assert np.allclose(tree.mass_stm, row["Ws"])
-        assert np.allclose(tree.mass_swd, row["Wss"])
-        # The R tmodel implementation slices off foliar respiration costs from
-        # GPP before doing anything - the pyrealm.tmodel implementation keeps
-        # this cost within the tree calculation
-        assert np.allclose(tree.gpp_actual * (1 - tree.traits.resp_f), row["GPP"])
-        assert np.allclose(tree.resp_swd, row["Rm1"])
-        assert np.allclose(tree.resp_frt, row["Rm2"])
-        assert np.allclose(tree.delta_mass_stm, row["dWs"])
-        assert np.allclose(tree.delta_mass_frt, row["dWfr"])
+#     # Some implementation differences
+#     if pyv == "delta_d":
+#         # The R tmodel implementation rescales reported delta_d as
+#         # a radial increase in millimetres.
+#         py_var *= 500
+#     elif pyv == "gpp_actual":
+#         # The R tmodel implementation slices off foliar respiration costs from
+#         # GPP before doing anything - the pyrealm.tmodel implementation keeps
+#         # this cost within the tree calculation
+#         py_var *= 1 - traits.resp_f
 
-
-@pytest.mark.parametrize(
-    "varname",
-    [
-        (0, ("dD", "delta_d")),
-        (1, ("D", "diameter")),
-        (2, ("H", "height")),
-        (3, ("Ac", "crown_area")),
-        (4, ("Wf", "mass_fol")),
-        (5, ("Ws", "mass_stm")),
-        (6, ("Wss", "mass_swd")),
-        (7, ("GPP", "gpp_actual")),
-        (8, ("Rm1", "resp_swd")),
-        (9, ("Rm2", "resp_frt")),
-        (10, ("dWs", "delta_mass_stm")),
-        (11, ("dWfr", "delta_mass_frt")),
-    ],
-)
-def test_grow_ttree(rvalues, pyvalues, varname):
-    """Runs a test of the tmodel.grow_ttree against the same R output.
-    In this case, the iteration process through time is tested. If the
-    previous test is successful then the values being fed forward in the
-    iteration _should_ be all fine, but this checks that the time iteration
-    is being run correctly in addition to the implementation of the model being
-    correct
-
-    Args:
-        values: Expected outputs from R implementation
-
-    """
-
-    idx, (rv, pyv) = varname
-    traits, pyvalues = pyvalues
-
-    # Get all the R values across timesteps into an array
-    r_var = np.array([rw[rv] for rw in rvalues])
-    # Get the matching py output
-    py_var = pyvalues[..., idx]
-
-    # Some implementation differences
-    if pyv == "delta_d":
-        # The R tmodel implementation rescales reported delta_d as
-        # a radial increase in millimetres.
-        py_var *= 500
-    elif pyv == "gpp_actual":
-        # The R tmodel implementation slices off foliar respiration costs from
-        # GPP before doing anything - the pyrealm.tmodel implementation keeps
-        # this cost within the tree calculation
-        py_var *= 1 - traits.resp_f
-
-    assert np.allclose(r_var, py_var)
+#     assert np.allclose(r_var, py_var)
