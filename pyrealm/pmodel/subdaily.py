7bea29e8c615ed71aa87aa18e23aaa171fe12ee4
David Orme <davidorme@users.noreply.github.com>
2023-03-21 11:31:31 +0000

    First versions of FastSlowPModel classes

--- pyrealm/pmodel/subdaily.py
+++ pyrealm/pmodel/subdaily.py
@@ -1,18 +1,52 @@
-"""Draft code for the subdaily PModel."""  # noqa: D205, D415
+r"""The :mod:`~pyrealm.pmodel.subdaily` module provides extensions to the P Model that
+incorporate modelling of the fast and slow responses of photosynthesis to changing
+conditions. The implementation is derived from the weighted-average approach presented
+in  {cite}`mengoli:2022a`.
 
-import bottleneck as bn  # type: ignore
+The core of the module is the :class:`~pyrealm.pmodel.subdaily.FastSlowPModel` class.
+This takes a standard :class:`~pyrealm.pmodel.pmodel.PModelEnvironment` object but
+additionally associates the first dimension of the data arrays in that object with a
+time series giving the sampling times of the observations. This time series is provided
+using a :class:`~pyrealm.pmodel.fast_slow_scaler.FastSlowScaler` instance, which is also
+used to set a daily acclimation window for the model. This acclimation window is set to
+capture the daily conditions under which the productivity of the plant will be
+optimised.
+
+The implementation uses the following workflow:
+
+* The daily acclimation window set in the
+  :class:`~pyrealm.pmodel.fast_slow_scaler.FastSlowScaler` instance is used to calculate
+  daily average values for forcing variables from within the acclimation window.
+* A standard P Model is then run on those daily forcing values to generate predicted
+  states for photosynthetic parameters that give rise to optimal productivity.
+* However, three key variables (:math:`\xi`, :math:`V_{cmax25}` and :math:`J_{max25}`)
+  do not respond instantaneously, but instead exhibit a slow response to changing
+  conditions, usually on a scale of around a fortnight. This lagged response is
+  implemented using a moving weighted average using the
+  :class:`~pyrealm.pmodel.subdaily.memory_effect` function.
+* These slowly responding variables are mapped back onto the original subdaily
+  timescale, with :math:`V_{cmax25}` and :math:`J_{max25}` also being corrected from
+  their values at a standard 25°C to the actual subdaily temperature.
+* Predictions of GPP are then made as in the standard P Model, but using the slowly
+  evolving values of :math:`\xi`, :math:`V_{cmax25}` and :math:`J_{max25}` and the
+  standard fast responses of other variables.
+"""  # noqa: D205, D415
+from typing import Optional
+
 import numpy as np
 from numpy.typing import NDArray
 
-from pyrealm.pmodel.functions import calc_ftemp_arrh
+from pyrealm.pmodel import PModel, PModelEnvironment, calc_ftemp_arrh, calc_ftemp_kphio
+from pyrealm.pmodel.fast_slow_scaler import FastSlowScaler
 
 
 def memory_effect(values: NDArray, alpha: float = 0.067) -> NDArray:
-    r"""Apply a memory effect to a time series.
+    r"""Apply a memory effect to a variable.
 
-    Vcmax and Jmax do not converge instantaneously to acclimated optimal values. This
-    function estimates how the actual Vcmax and Jmax track a time series of calculated
-    optimal values assuming instant acclimation.
+    Three key photosynthetic parameters (:math:`\xi`, :math:`V_{cmax25}` and
+    :math:`J_{max25}`) show slow responses to changing environmental conditions and do
+    not instantaneously adopt optimal values. This function applies a rolling weighted
+    average to apply a lagged response to one of these parameters.
 
     The estimation uses the paramater `alpha` (:math:`\alpha`) to control the speed of
     convergence of the estimated values (:math:`E`) to the calculated optimal values
@@ -35,7 +69,7 @@ def memory_effect(values: NDArray, alpha: float = 0.067) -> NDArray:
 
     # TODO - NA handling
     # TODO - think about filters here - I'm sure this is a filter which
-    #        could generalise to longer memory windows.
+    #        could generalise to longer memory windows (convolve?)
     # TODO - need a version that handles time slices for low memory looping
     #        over arrays.
 
@@ -48,33 +82,339 @@ def memory_effect(values: NDArray, alpha: float = 0.067) -> NDArray:
     return memory_values
 
 
-def interpolate_rates_forward(
-    tk: NDArray, ha: float, values: NDArray, values_idx: NDArray
-) -> NDArray:
-    """Interpolate Jmax and Vcmax forward in time.
+class FastSlowPModel:
+    r"""Fit a P Model incorporating fast and slow photosynthetic responses.
 
-    This is a specialised interpolation function used for Jmax and Vcmax. Given a time
-    series of temperatures in Kelvin (`tk`) and a set of Jmax25 or Vcmax25 values
-    observed at indices (`values_idx`) along that time series, this pushes those values
-    along the time series and then rescales to the observed temperatures.
+    TODO
 
-    The effect is that the plant 'sets' its response at a given point of the day and
-    then maintains that same behaviour until a similar reference time the following day.
+    Args:
+        env: An instance of :class:`~pyrealm.pmodel.pmodel.PModelEnvironment`.
+        fs_scaler: An instance of :class:`~pyrealm.pmodel.subdaily.FastSlowScaler`, with
+            an acclimation window set using one of the ``set_`` methods.
+        kphio: (Optional) The quantum yield efficiency of photosynthesis
+            (:math:`\phi_0`, unitless).
 
-    Note that the beginning of the sequence will be filled with np.nan values unless
-    values_idx[0] = 0.
 
-    Arguments:
-        tk: A time series of temperature values (Kelvin).
-        ha: An Arrhenius constant.
-        values: An array of rates at standard temperature predicted at points along tk.
-        values_idx: The indices of tk at which values are predicted.
+    Examples:
+        >>> TODO
     """
 
-    v = np.empty_like(tk)
-    v[:] = np.nan
+    def __init__(
+        self,
+        env: PModelEnvironment,
+        fs_scaler: FastSlowScaler,
+        ppfd: NDArray,
+        fapar: NDArray,
+        alpha: float = 1 / 15,
+        kphio: float = 1 / 8,
+        vpd_scaler: Optional[FastSlowScaler] = None,
+        fill_kind: str = "previous",
+    ) -> None:
+        # Check that the length of the fast slow scaler is congruent with the
+        # first axis of the photosynthetic environment
+        n_datetimes = fs_scaler.datetimes.shape[0]
+        n_env_first_axis = env.tc.shape[0]
 
-    v[values_idx] = values
-    v = bn.push(v)
+        if n_datetimes != n_env_first_axis:
+            raise ValueError("env and fs_scaler do not have congruent dimensions")
 
-    return v * calc_ftemp_arrh(tk=tk, ha=ha)
+        # Has a set method been run on the fast slow scaler
+        if not hasattr(fs_scaler, "include"):
+            raise ValueError("The daily sampling window has not been set on fs_scaler")
+
+        self.env = env
+        # Get the daily estimates of the acclimation targets for forcing variables
+        temp_acclim = fs_scaler.get_daily_means(self.env.tc)
+        co2_acclim = fs_scaler.get_daily_means(self.env.co2)
+        patm_acclim = fs_scaler.get_daily_means(self.env.patm)
+
+        if vpd_scaler is not None:
+            vpd_acclim = vpd_scaler.get_daily_means(self.env.vpd)
+        else:
+            vpd_acclim = fs_scaler.get_daily_means(self.env.vpd)
+
+        # TODO - calculate the acclimated daily model using GPP per unit Iabs and then
+        #        scale up to subdaily variation in fapar and ppfd at the endrun
+        self.ppfd_acclim = fs_scaler.get_daily_means(ppfd)
+        self.fapar_acclim = fs_scaler.get_daily_means(fapar)
+
+        # Calculate the PModelEnvironment for those conditions and then the PModel
+        # itself to obtain estimates of jmax and vcmax
+        pmodel_env_acclim = PModelEnvironment(
+            tc=temp_acclim,
+            vpd=vpd_acclim,
+            co2=co2_acclim,
+            patm=patm_acclim,
+            const=self.env.const,
+        )
+        self.pmodel_acclim: PModel = PModel(pmodel_env_acclim, kphio=kphio)
+        r"""P Model predictions for the daily acclimation conditions.
+
+        A :class:`~pyrealm.pmodel.pmodel.PModel` instance providing the predictions of
+        the P Model for the daily acclimation conditions set for the FastSlowPModel. The
+        model predicts instantaneous optimal estimates of :math:`V_{cmax}`,
+        :math:`J_max` and `:math:`\xi`, which are then used to estimate realised values
+        of those parameters given slow responses to acclimation.
+        """
+
+        # Calculate productivity measures including jmax and vcmax
+        self.pmodel_acclim.estimate_productivity(
+            fapar=self.fapar_acclim, ppfd=self.ppfd_acclim
+        )
+
+        # Calculate the optimal jmax and vcmax at 25°C
+        # TODO - Are these any of the existing values in the constants?
+        ha_vcmax25 = 65330
+        ha_jmax25 = 43900
+
+        tk_acclim = temp_acclim + self.env.const.k_CtoK
+        self.vcmax25_opt = self.pmodel_acclim.vcmax * (
+            1 / calc_ftemp_arrh(tk_acclim, ha_vcmax25)
+        )
+        self.jmax25_opt = self.pmodel_acclim.jmax * (
+            1 / calc_ftemp_arrh(tk_acclim, ha_jmax25)
+        )
+
+        # Calculate the realised values from the instantaneous optimal values
+        self.xi_real: NDArray = memory_effect(self.pmodel_acclim.optchi.xi, alpha=alpha)
+        r"""Realised daily slow responses in :math:`\xi`"""
+        self.vcmax25_real: NDArray = memory_effect(self.vcmax25_opt, alpha=alpha)
+        r"""Realised daily slow responses in :math:`V_{cmax25}`"""
+        self.jmax25_real: NDArray = memory_effect(self.jmax25_opt, alpha=alpha)
+        r"""Realised daily slow responses in :math:`J_{max25}`"""
+
+        # Fill the daily realised values onto the subdaily scale
+        subdaily_tk = self.env.tc + self.env.const.k_CtoK
+
+        # Fill the realised xi, jmax25 and vcmax25 from subdaily to daily and then
+        # adjust jmax25 and vcmax25 to jmax and vcmax given actual temperature at
+        # subdaily timescale
+        self.subdaily_vcmax25 = fs_scaler.fill_daily_to_subdaily(self.vcmax25_real)
+        self.subdaily_jmax25 = fs_scaler.fill_daily_to_subdaily(self.jmax25_real)
+        self.subdaily_xi = fs_scaler.fill_daily_to_subdaily(self.xi_real)
+
+        self.subdaily_vcmax: NDArray = self.subdaily_vcmax25 * calc_ftemp_arrh(
+            tk=subdaily_tk, ha=ha_vcmax25
+        )
+        """Estimated subdaily :math:`V_{cmax}`."""
+
+        self.subdaily_jmax: NDArray = self.subdaily_jmax25 * calc_ftemp_arrh(
+            tk=subdaily_tk, ha=ha_jmax25
+        )
+        """Estimated subdaily :math:`J_{max}`."""
+
+        self.subdaily_ci: NDArray = (
+            self.subdaily_xi * self.env.ca + self.env.gammastar * np.sqrt(self.env.vpd)
+        ) / (self.subdaily_xi + np.sqrt(self.env.vpd))
+        """Estimated subdaily :math:`c_i`."""
+
+        # Calculate Ac, J and Aj at subdaily scale to calculate assimilation
+        self.subdaily_Ac: NDArray = (
+            self.subdaily_vcmax
+            * (self.subdaily_ci - self.env.gammastar)
+            / (self.subdaily_ci + self.env.kmm)
+        )
+        """Estimated subdaily :math:`A_c`."""
+
+        kphio_tc = kphio * calc_ftemp_kphio(tc=self.env.tc)
+        iabs = fapar * ppfd
+
+        subdaily_J = (4 * kphio_tc * iabs) / np.sqrt(
+            1 + ((4 * kphio_tc * iabs) / self.subdaily_jmax) ** 2
+        )
+
+        self.subdaily_Aj: NDArray = (
+            (subdaily_J / 4)
+            * (self.subdaily_ci - self.env.gammastar)
+            / (self.subdaily_ci + 2 * self.env.gammastar)
+        )
+        """Estimated subdaily :math:`A_j`."""
+
+        # Calculate light use efficiency
+        self.gpp: NDArray = np.minimum(self.subdaily_Aj, self.subdaily_Ac)
+        """Estimated subdaily GPP."""
+
+    # def estimate_gpp(self, fapar: NDArray, ppfd: NDArray) -> None:
+    #     """Estimate productivity"""
+    #     iabs = fapar * ppfd
+
+    #     self.gpp = self.lue * iabs
+
+
+class FastSlowPModel_JAMES:
+    r"""Fit a P Model incorporating fast and slow photosynthetic responses.
+
+    * The optimal daily values are calculated using a different window for VPD, which
+      takes the exact noon value rather than the mean of the noon window.
+    * The daily fAPAR values are also not the same as the mean of the noon window
+    * The subdaily values of Jmax25 and Vcmax25 are filled forward from noon, rather
+      than the end of the noon window, which predicts the future.
+
+    Args:
+        env: An instance of :class:`~pyrealm.pmodel.pmodel.PModelEnvironment`.
+        fs_scaler: An instance of :class:`~pyrealm.pmodel.subdaily.FastSlowScaler`.
+        kphio: (Optional) The quantum yield efficiency of photosynthesis
+            (:math:`\phi_0`, unitless). Note that :math:`\phi_0` is sometimes used to
+            refer to the quantum yield of electron transfer, which is exactly four times
+            larger, so check definitions here.
+
+    Examples:
+        >>> TODO
+    """
+
+    def __init__(
+        self,
+        env: PModelEnvironment,
+        fs_scaler: FastSlowScaler,
+        ppfd: NDArray,
+        fapar: NDArray,
+        alpha: float = 1 / 15,
+        kphio: float = 1 / 8,
+        vpd_scaler: Optional[FastSlowScaler] = None,
+        fapar_acclim: Optional[NDArray] = None,
+        fill_from: Optional[np.timedelta64] = None,
+        fill_kind: str = "previous",
+    ) -> None:
+        # Check that the length of the fast slow scaler is congruent with the
+        # first axis of the photosynthetic environment
+        n_datetimes = fs_scaler.datetimes.shape[0]
+        n_env_first_axis = env.tc.shape[0]
+
+        if n_datetimes != n_env_first_axis:
+            raise ValueError("env and fs_scaler do not have congruent dimensions")
+
+        # Has a set method been run on the fast slow scaler
+        if not hasattr(fs_scaler, "include"):
+            raise ValueError("The daily sampling window has not been set on fs_scaler")
+
+        self.env = env
+        # Get the daily estimates of the acclimation targets for forcing variables
+        temp_acclim = fs_scaler.get_daily_means(self.env.tc)
+        co2_acclim = fs_scaler.get_daily_means(self.env.co2)
+        patm_acclim = fs_scaler.get_daily_means(self.env.patm)
+
+        if vpd_scaler is not None:
+            vpd_acclim = vpd_scaler.get_daily_means(self.env.vpd)
+        else:
+            vpd_acclim = fs_scaler.get_daily_means(self.env.vpd)
+
+        # TODO - calculate the acclimated daily model using GPP per unit Iabs and then
+        #        scale up to subdaily variation in fapar and ppfd at the endrun
+        self.ppfd_acclim = fs_scaler.get_daily_means(ppfd)
+        if fapar_acclim is not None:
+            self.fapar_acclim = fapar_acclim
+        else:
+            self.fapar_acclim = fs_scaler.get_daily_means(fapar)
+
+        # Calculate the PModelEnvironment for those conditions and then the PModel
+        # itself to obtain estimates of jmax and vcmax
+        pmodel_env_acclim = PModelEnvironment(
+            tc=temp_acclim,
+            vpd=vpd_acclim,
+            co2=co2_acclim,
+            patm=patm_acclim,
+            const=self.env.const,
+        )
+        self.pmodel_acclim: PModel = PModel(pmodel_env_acclim, kphio=kphio)
+        r"""P Model predictions for the daily acclimation conditions.
+
+        A :class:`~pyrealm.pmodel.pmodel.PModel` instance providing the predictions of
+        the P Model for the daily acclimation conditions set for the FastSlowPModel. The
+        model predicts instantaneous optimal estimates of :math:`V_{cmax}`,
+        :math:`J_max` and `:math:`\xi`, which are then used to estimate realised values
+        of those parameters given slow responses to acclimation.
+        """
+
+        # Calculate productivity measures including jmax and vcmax
+        self.pmodel_acclim.estimate_productivity(
+            fapar=self.fapar_acclim, ppfd=self.ppfd_acclim
+        )
+
+        # Calculate the optimal jmax and vcmax at 25°C
+        # TODO - Are these any of the existing values in the constants?
+        ha_vcmax25 = 65330
+        ha_jmax25 = 43900
+
+        tk_acclim = temp_acclim + self.env.const.k_CtoK
+        self.vcmax25_opt = self.pmodel_acclim.vcmax * (
+            1 / calc_ftemp_arrh(tk_acclim, ha_vcmax25)
+        )
+        self.jmax25_opt = self.pmodel_acclim.jmax * (
+            1 / calc_ftemp_arrh(tk_acclim, ha_jmax25)
+        )
+
+        # Calculate the realised values from the instantaneous optimal values
+        self.vcmax25_real: NDArray = memory_effect(self.vcmax25_opt, alpha=alpha)
+        r"""Realised daily slow responses in :math:`V_{cmax25}`"""
+        self.jmax25_real: NDArray = memory_effect(self.jmax25_opt, alpha=alpha)
+        r"""Realised daily slow responses in :math:`J_{max25}`"""
+
+        # Calculate the daily xi value, which does not have a slow reponse in this
+        # implementation.
+        # - Calculate subdaily time series for gammastar, xi and ca, filled forwards
+        #   from midnight
+        subdaily_gammastar = fs_scaler.fill_daily_to_subdaily(
+            self.pmodel_acclim.env.gammastar, fill_from=np.timedelta64(0, "h")
+        )
+        subdaily_xi = fs_scaler.fill_daily_to_subdaily(
+            self.pmodel_acclim.optchi.xi, fill_from=np.timedelta64(0, "h")
+        )
+        subdaily_ca = fs_scaler.fill_daily_to_subdaily(
+            self.pmodel_acclim.env.ca, fill_from=np.timedelta64(0, "h")
+        )
+
+        # Calculate ci using the daily optimal acclimated values for xi, ca and
+        # gammastar and the actual daily variation in VPD.
+        self.subdaily_ci = (
+            subdaily_xi * subdaily_ca + subdaily_gammastar * np.sqrt(self.env.vpd)
+        ) / (subdaily_xi + np.sqrt(self.env.vpd))
+        """Estimated subdaily :math:`c_i`."""
+
+        # Fill the daily realised values onto the subdaily scale
+        subdaily_tk = self.env.tc + self.env.const.k_CtoK
+
+        # Fill the realised xi, jmax25 and vcmax25 from subdaily to daily and then
+        # adjust jmax25 and vcmax25 to jmax and vcmax given actual temperature at
+        # subdaily timescale
+        self.subdaily_vcmax25 = fs_scaler.fill_daily_to_subdaily(
+            self.vcmax25_real, fill_from=fill_from
+        )
+        self.subdaily_jmax25 = fs_scaler.fill_daily_to_subdaily(
+            self.jmax25_real, fill_from=fill_from
+        )
+
+        self.subdaily_vcmax: NDArray = self.subdaily_vcmax25 * calc_ftemp_arrh(
+            tk=subdaily_tk, ha=ha_vcmax25
+        )
+        """Estimated subdaily :math:`V_{cmax}`."""
+
+        self.subdaily_jmax: NDArray = self.subdaily_jmax25 * calc_ftemp_arrh(
+            tk=subdaily_tk, ha=ha_jmax25
+        )
+        """Estimated subdaily :math:`J_{max}`."""
+
+        # Calculate Ac, J and Aj at subdaily scale to calculate assimilation
+        self.subdaily_Ac: NDArray = (
+            self.subdaily_vcmax
+            * (self.subdaily_ci - self.env.gammastar)
+            / (self.subdaily_ci + self.env.kmm)
+        )
+        """Estimated subdaily :math:`A_c`."""
+
+        kphio_tc = kphio * calc_ftemp_kphio(tc=self.env.tc)
+        iabs = fapar * ppfd
+
+        subdaily_J = (4 * kphio_tc * iabs) / np.sqrt(
+            1 + ((4 * kphio_tc * iabs) / self.subdaily_jmax) ** 2
+        )
+
+        self.subdaily_Aj: NDArray = (
+            (subdaily_J / 4)
+            * (self.subdaily_ci - self.env.gammastar)
+            / (self.subdaily_ci + 2 * self.env.gammastar)
+        )
+        """Estimated subdaily :math:`A_j`."""
+
+        # Calculate light use efficiency
+        self.gpp: NDArray = np.minimum(self.subdaily_Aj, self.subdaily_Ac)
+        """Estimated subdaily GPP."""
