ae04506edc74161fd55d9ddfbea163c0f2f2555f
David Orme <davidorme@users.noreply.github.com>
2023-03-27 11:05:19 +0100

    Moving boilerplate in test FastSlowPmodel tests

--- tests/pmodel/test_fast_slow_pmodel.py
+++ tests/pmodel/test_fast_slow_pmodel.py
@@ -6,6 +6,10 @@ import pytest
 
 @pytest.fixture
 def be_vie_data():
+    """Import the test data and provide it as a PModelEnv and two arrays"""
+
+    from pyrealm.pmodel import PModelEnvironment
+
     with resources.path("data", "subdaily_BE_Vie_2014.csv") as data_path:
         data = np.genfromtxt(
             data_path,
@@ -16,54 +20,52 @@ def be_vie_data():
             missing_values="NA",
         )
 
-    return data
+    # Extract the key half hourly timestep variables
+    ppfd_subdaily = data["ppfd"]
+    fapar_subdaily = data["fapar"]
+    datetime_subdaily = data["time"].astype(np.datetime64)
+    expected_gpp = data["GPP_JAMES"]
+
+    # Create the environment
+    subdaily_env = PModelEnvironment(
+        tc=data["ta"],
+        vpd=data["vpd"],
+        co2=data["co2"],
+        patm=data["patm"],
+    )
+
+    return subdaily_env, ppfd_subdaily, fapar_subdaily, datetime_subdaily, expected_gpp
 
 
 def test_FSPModel_JAMES(be_vie_data):
     """This tests the legacy calculations from the Mengoli et al JAMES paper, using that
     version of the weighted average calculations without acclimating xi."""
 
-    from pyrealm.pmodel import FastSlowScaler, PModelEnvironment
+    from pyrealm.pmodel import FastSlowScaler
     from pyrealm.pmodel.subdaily import FastSlowPModel_JAMES
 
-    # Extract the key half hourly timestep variables
-    temp_subdaily = be_vie_data["ta"]
-    vpd_subdaily = be_vie_data["vpd"]
-    co2_subdaily = be_vie_data["co2"]
-    patm_subdaily = be_vie_data["patm"]
-    ppfd_subdaily = be_vie_data["ppfd"]
-    fapar_subdaily = be_vie_data["fapar"]
-    datetime_subdaily = be_vie_data["time"].astype(np.datetime64)
-    expected_gpp = be_vie_data["GPP_JAMES"]
-
-    # Create the environment
-    subdaily_env = PModelEnvironment(
-        tc=temp_subdaily,
-        vpd=vpd_subdaily,
-        co2=co2_subdaily,
-        patm=patm_subdaily,
-    )
+    env, ppfd, fapar, datetime, expected_gpp = be_vie_data
 
     # Get the fast slow scaler and set window
-    fsscaler = FastSlowScaler(datetime_subdaily)
+    fsscaler = FastSlowScaler(datetime)
     fsscaler.set_window(
         window_center=np.timedelta64(12, "h"),
         half_width=np.timedelta64(30, "m"),
     )
 
     # Alternate scalar used to duplicate VPD settings in JAMES implementation
-    vpdscaler = FastSlowScaler(datetime_subdaily)
+    vpdscaler = FastSlowScaler(datetime)
     vpdscaler.set_nearest(time=np.timedelta64(12, "h"))
 
     # Fast slow model without acclimating xi with best fit adaptations to the original
     # - VPD in daily optimum using different window
     # - Jmax and Vcmax filling from midday not window end
     fs_pmodel_james = FastSlowPModel_JAMES(
-        env=subdaily_env,
+        env=env,
         fs_scaler=fsscaler,
         kphio=1 / 8,
-        fapar=fapar_subdaily,
-        ppfd=ppfd_subdaily,
+        fapar=fapar,
+        ppfd=ppfd,
         vpd_scaler=vpdscaler,
         fill_from=np.timedelta64(12, "h"),
     )
