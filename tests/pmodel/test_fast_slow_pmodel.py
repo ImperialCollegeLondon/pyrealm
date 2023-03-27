ad4861b9c4209784d5629d334f980bcff10f3681
David Orme <davidorme@users.noreply.github.com>
2023-03-27 13:58:34 +0100

    Updated tests - added input dimensionality check on FastSlowPModel

--- tests/pmodel/test_fast_slow_pmodel.py
+++ tests/pmodel/test_fast_slow_pmodel.py
@@ -4,11 +4,9 @@ import numpy as np
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def be_vie_data():
-    """Import the test data and provide it as a PModelEnv and two arrays"""
-
-    from pyrealm.pmodel import PModelEnvironment
+    """Import the test data"""
 
     with resources.path("data", "subdaily_BE_Vie_2014.csv") as data_path:
         data = np.genfromtxt(
@@ -20,31 +18,40 @@ def be_vie_data():
             missing_values="NA",
         )
 
+    return data
+
+
+@pytest.fixture(scope="module")
+def be_vie_data_components(be_vie_data):
+    """Convert the test data into a PModelEnv and arrays"""
+
+    from pyrealm.pmodel import PModelEnvironment
+
     # Extract the key half hourly timestep variables
-    ppfd_subdaily = data["ppfd"]
-    fapar_subdaily = data["fapar"]
-    datetime_subdaily = data["time"].astype(np.datetime64)
-    expected_gpp = data["GPP_JAMES"]
+    ppfd_subdaily = be_vie_data["ppfd"]
+    fapar_subdaily = be_vie_data["fapar"]
+    datetime_subdaily = be_vie_data["time"].astype(np.datetime64)
+    expected_gpp = be_vie_data["GPP_JAMES"]
 
     # Create the environment
     subdaily_env = PModelEnvironment(
-        tc=data["ta"],
-        vpd=data["vpd"],
-        co2=data["co2"],
-        patm=data["patm"],
+        tc=be_vie_data["ta"],
+        vpd=be_vie_data["vpd"],
+        co2=be_vie_data["co2"],
+        patm=be_vie_data["patm"],
     )
 
     return subdaily_env, ppfd_subdaily, fapar_subdaily, datetime_subdaily, expected_gpp
 
 
-def test_FSPModel_JAMES(be_vie_data):
+def test_FSPModel_JAMES(be_vie_data_components):
     """This tests the legacy calculations from the Mengoli et al JAMES paper, using that
     version of the weighted average calculations without acclimating xi."""
 
     from pyrealm.pmodel import FastSlowScaler
     from pyrealm.pmodel.subdaily import FastSlowPModel_JAMES
 
-    env, ppfd, fapar, datetime, expected_gpp = be_vie_data
+    env, ppfd, fapar, datetime, expected_gpp = be_vie_data_components
 
     # Get the fast slow scaler and set window
     fsscaler = FastSlowScaler(datetime)
@@ -79,14 +86,13 @@ def test_FSPModel_JAMES(be_vie_data):
     assert np.allclose(fs_pmodel_james.gpp[valid], expected_gpp[valid], rtol=0.005)
 
 
-def test_FSPModel_corr(be_vie_data):
+def test_FSPModel_corr(be_vie_data_components):
     """This tests the pyrealm implementation correlates well with the legacy
     calculations from the Mengoli et al JAMES paper without acclimating xi."""
 
-    from pyrealm.pmodel import FastSlowScaler
-    from pyrealm.pmodel.subdaily import FastSlowPModel
+    from pyrealm.pmodel import FastSlowPModel, FastSlowScaler
 
-    env, ppfd, fapar, datetime, expected_gpp = be_vie_data
+    env, ppfd, fapar, datetime, expected_gpp = be_vie_data_components
 
     # Get the fast slow scaler and set window
     fsscaler = FastSlowScaler(datetime)
@@ -96,7 +102,7 @@ def test_FSPModel_corr(be_vie_data):
     )
 
     # Fast slow model
-    fs_pmodel_james = FastSlowPModel(
+    fs_pmodel = FastSlowPModel(
         env=env,
         fs_scaler=fsscaler,
         kphio=1 / 8,
@@ -105,9 +111,64 @@ def test_FSPModel_corr(be_vie_data):
     )
 
     valid = np.logical_not(
-        np.logical_or(np.isnan(expected_gpp), np.isnan(fs_pmodel_james.gpp))
+        np.logical_or(np.isnan(expected_gpp), np.isnan(fs_pmodel.gpp))
     )
 
     # Test that non-NaN predictions correlate well
-    r_vals = np.corrcoef(fs_pmodel_james.gpp[valid], expected_gpp[valid])
+    r_vals = np.corrcoef(fs_pmodel.gpp[valid], expected_gpp[valid])
     assert np.alltrue(r_vals > 0.995)
+
+
+@pytest.mark.parametrize("ndims", [2, 3, 4])
+def test_FSPModel_dimensionality(be_vie_data, ndims):
+    """This tests that the FastSlowPModel handles dimensions correctly.
+
+    This broadcasts the BE-Vie onto more dimensions and checks that the code iterates
+    over those dimensions correctly. fAPAR and PPFD are then fixed across the other
+    dimensions to check the results scale as expected.
+    """
+
+    from pyrealm.pmodel import FastSlowPModel, FastSlowScaler, PModelEnvironment
+
+    datetime = be_vie_data["time"].astype(np.datetime64)
+
+    # Set up the dimensionality for the test - create a shape tuple with extra
+    # dimensions and then broadcast the model inputs onto it. These need to be
+    # transposed to return datetime to the first axis. When n_dim = 1, the data are
+    # passed as is.
+    extra_dims = [3] * (ndims - 1)
+    array_dims = tuple(extra_dims + [len(datetime)])
+
+    # Create the environment
+    env = PModelEnvironment(
+        tc=np.broadcast_to(be_vie_data["ta"], array_dims).transpose(),
+        vpd=np.broadcast_to(be_vie_data["vpd"], array_dims).transpose(),
+        co2=np.broadcast_to(be_vie_data["co2"], array_dims).transpose(),
+        patm=np.broadcast_to(be_vie_data["patm"], array_dims).transpose(),
+    )
+
+    # Get the fast slow scaler and set window
+    fsscaler = FastSlowScaler(datetime)
+    fsscaler.set_window(
+        window_center=np.timedelta64(12, "h"),
+        half_width=np.timedelta64(30, "m"),
+    )
+
+    # Apply a different random value of fAPAR for each time series
+    fapar_vals = np.random.random(extra_dims)
+    fapar_vals[0] = 1.0
+
+    # Fast slow model
+    fs_pmodel = FastSlowPModel(
+        env=env,
+        fs_scaler=fsscaler,
+        kphio=1 / 8,
+        fapar=fapar_vals * np.ones(array_dims).transpose(),
+        ppfd=np.ones(array_dims).transpose(),
+    )
+
+    # The GPP along the timescale of the different dimensions should be directly
+    # proportional to the random fapar values and hence GPP/FAPAR should all equal the
+    # value when it is set to 1
+    timeaxis_mean = np.nansum(fs_pmodel.gpp, axis=0)
+    assert np.allclose(timeaxis_mean / fapar_vals, timeaxis_mean[0])
