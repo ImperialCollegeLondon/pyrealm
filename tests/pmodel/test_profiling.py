c61a8249c7b8beb49b052f67a69fccca3f2022c0
David Orme <davidorme@users.noreply.github.com>
2023-08-29 15:37:47 +0100

    Extended PModel profiling to include a simple C4 model

--- tests/pmodel/test_profiling.py
+++ tests/pmodel/test_profiling.py
@@ -1,14 +1,16 @@
 import pytest
 
-def test_profiling_example():
 
+def test_profiling_example():
     from importlib import resources
-    import xarray
+
     import numpy as np
+    import xarray
+
     from pyrealm.pmodel import PModel, PModelEnvironment
 
     # Loading the dataset:
-    dpath = resources.files("pyrealm_build_data") / "inputs_data_24.25.nc"   
+    dpath = resources.files("pyrealm_build_data") / "inputs_data_24.25.nc"
 
     ds = xarray.load_dataset(dpath)
 
@@ -30,7 +32,12 @@ def test_profiling_example():
     # Generate and check the PModelEnvironment
     pm_env = PModelEnvironment(tc=tc, patm=patm, vpd=vpd, co2=co2)
 
-    # Standard PModel
-    pmod = PModel(env=pm_env, kphio=1 / 8)
-    pmod.estimate_productivity(fapar=fapar, ppfd=ppfd)
-    pmod.summarize()
+    # Standard C3 PModel
+    pmod_c3 = PModel(env=pm_env, kphio=1 / 8)
+    pmod_c3.estimate_productivity(fapar=fapar, ppfd=ppfd)
+    pmod_c3.summarize()
+
+    # Standard C4 PModel
+    pmod_c4 = PModel(env=pm_env, kphio=1 / 8, method_optchi="c4")
+    pmod_c4.estimate_productivity(fapar=fapar, ppfd=ppfd)
+    pmod_c4.summarize()
