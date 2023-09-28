01ee9770732e7f3b9b454e6c33a2c2e90b934331
Alexander Smith <a.smith.inbox@googlemail.com>
2023-09-28 12:02:32 +0100

    Split profiling tests

--- tests/pmodel/test_profiling.py
+++ tests/pmodel/test_profiling.py
@@ -1,122 +1,135 @@
 import pytest
+from importlib import resources
+import numpy as np
+import xarray
+from pyrealm.pmodel import PModelEnvironment
+
+class TestClass:
+
+    @pytest.fixture()
+    def setup(self):
+
+        # Loading the dataset:
+        dpath = resources.files("pyrealm_build_data") / "inputs_data_24.25.nc"
+
+        ds = xarray.load_dataset(dpath)
+
+        # TODO - this is a bit of a hack because of the current unfriendly handling of data
+        #        that does not form neat blocks of daily data in the subdaily module. The
+        #        test data is a slice along longitude 24.25°E (Finland --> Crete -->
+        #        Botswana) so the actual local times needed for the subdaily module are
+        #        offset from the UTC times in the data. This step reduces the input data to
+        #        complete daily blocks of data using local time
+
+        ds = ds.sel(time=slice("2000-01-01T01:59", "2001-12-31T01:59"))
+        self.local_offset = np.timedelta64(int((24.25 * (24 / 360)) * 60 * 60), "s")
+        self.local_time = ds["time"].to_numpy() - self.local_offset
+
+        # Variable set up
+        # Air temperature in Kelvin
+        self.tc = (ds["temp"]).to_numpy()
+        # Atmospheric pressure in Pascals
+        self.patm = ds["patm"].to_numpy()
+        # Obtain VPD and remove negative values
+        self.vpd = ds["vpd"].to_numpy()
+        self.vpd = np.clip(self.vpd, 0, np.inf)
+        # Extract fAPAR (unitless)
+        self.fapar = ds["fapar"].to_numpy()
+        # Gather PPFD µmole/m2/s1
+        self.ppfd = ds["ppfd"].to_numpy()
+        # Define atmospheric CO2 concentration (ppm)
+        self.co2 = np.ones_like(self.tc) * 400
 
 
-def test_profiling_example():
-    from importlib import resources
+        # Generate and check the PModelEnvironment
+        self.pm_env = PModelEnvironment(tc=self.tc, patm=self.patm, vpd=self.vpd, co2=self.co2)
 
-    import numpy as np
-    import xarray
 
-    from pyrealm.pmodel import (
-        C3C4Competition,
-        CalcCarbonIsotopes,
-        FastSlowPModel,
-        FastSlowScaler,
-        PModel,
-        PModelEnvironment,
-    )
+    def test_profiling_example(self, setup):
 
-    # Loading the dataset:
-    dpath = resources.files("pyrealm_build_data") / "inputs_data_24.25.nc"
+        from pyrealm.pmodel import (
+            C3C4Competition,
+            CalcCarbonIsotopes,
+            PModel,
+        )
 
-    ds = xarray.load_dataset(dpath)
 
-    # TODO - this is a bit of a hack because of the current unfriendly handling of data
-    #        that does not form neat blocks of daily data in the subdaily module. The
-    #        test data is a slice along longitude 24.25°E (Finland --> Crete -->
-    #        Botswana) so the actual local times needed for the subdaily module are
-    #        offset from the UTC times in the data. This step reduces the input data to
-    #        complete daily blocks of data using local time
+        # Profiling the PModel submodule
+        # Standard C3 PModel
+        pmod_c3 = PModel(env=self.pm_env, kphio=1 / 8)
+        pmod_c3.estimate_productivity(fapar=self.fapar, ppfd=self.ppfd)
+        pmod_c3.summarize()
 
-    ds = ds.sel(time=slice("2000-01-01T01:59", "2001-12-31T01:59"))
-    local_offset = np.timedelta64(int((24.25 * (24 / 360)) * 60 * 60), "s")
-    local_time = ds["time"].to_numpy() - local_offset
+        # Standard C4 PModel
+        pmod_c4 = PModel(env=self.pm_env, kphio=1 / 8, method_optchi="c4")
+        pmod_c4.estimate_productivity(fapar=self.fapar, ppfd=self.ppfd)
+        pmod_c4.summarize()
 
-    # Variable set up
-    # Air temperature in Kelvin
-    tc = (ds["temp"]).to_numpy()
-    # Atmospheric pressure in Pascals
-    patm = ds["patm"].to_numpy()
-    # Obtain VPD and remove negative values
-    vpd = ds["vpd"].to_numpy()
-    vpd = np.clip(vpd, 0, np.inf)
-    # Extract fAPAR (unitless)
-    fapar = ds["fapar"].to_numpy()
-    # Gather PPFD µmole/m2/s1
-    ppfd = ds["ppfd"].to_numpy()
-    # Define atmospheric CO2 concentration (ppm)
-    co2 = np.ones_like(tc) * 400
+        # Profiling the Competition submodule
+        # Competition, using annual GPP from µgC m2 s to g m2 yr
+        gpp_c3_annual = pmod_c3.gpp * (60 * 60 * 24 * 365) * 1e-6
+        gpp_c4_annual = pmod_c4.gpp * (60 * 60 * 24 * 365) * 1e-6
 
-    # Generate and check the PModelEnvironment
-    pm_env = PModelEnvironment(tc=tc, patm=patm, vpd=vpd, co2=co2)
+        # Fit the competition model - making some extrenely poor judgements about what is
+        # cropland and what is below the minimum temperature that really should be fixed.
+        comp = C3C4Competition(
+            gpp_c3=gpp_c3_annual,
+            gpp_c4=gpp_c4_annual,
+            treecover=np.array([0.5]),
+            below_t_min=np.full_like(self.pm_env.tc, False, dtype="bool"),
+            cropland=np.full_like(self.pm_env.tc, False, dtype="bool"),
+        )
 
-    # Profiling the PModel submodule
-    # Standard C3 PModel
-    pmod_c3 = PModel(env=pm_env, kphio=1 / 8)
-    pmod_c3.estimate_productivity(fapar=fapar, ppfd=ppfd)
-    pmod_c3.summarize()
+        comp.summarize()
 
-    # Standard C4 PModel
-    pmod_c4 = PModel(env=pm_env, kphio=1 / 8, method_optchi="c4")
-    pmod_c4.estimate_productivity(fapar=fapar, ppfd=ppfd)
-    pmod_c4.summarize()
+        # Profiling the isotopes submodule
+        # Create some entirely constant atmospheric isotope ratios
+        constant_d13CO2 = np.array([-8.4])
+        constant_D14CO2 = np.array([19.2])
 
-    # Profiling the Competition submodule
-    # Competition, using annual GPP from µgC m2 s to g m2 yr
-    gpp_c3_annual = pmod_c3.gpp * (60 * 60 * 24 * 365) * 1e-6
-    gpp_c4_annual = pmod_c4.gpp * (60 * 60 * 24 * 365) * 1e-6
+        # Calculate for the C3 model
+        isotope_c3 = CalcCarbonIsotopes(
+            pmod_c3, d13CO2=constant_d13CO2, D14CO2=constant_D14CO2
+        )
+        isotope_c3.summarize()
 
-    # Fit the competition model - making some extrenely poor judgements about what is
-    # cropland and what is below the minimum temperature that really should be fixed.
-    comp = C3C4Competition(
-        gpp_c3=gpp_c3_annual,
-        gpp_c4=gpp_c4_annual,
-        treecover=np.array([0.5]),
-        below_t_min=np.full_like(pm_env.tc, False, dtype="bool"),
-        cropland=np.full_like(pm_env.tc, False, dtype="bool"),
-    )
+        # Calculate for the C4 model
+        isotope_c4 = CalcCarbonIsotopes(
+            pmod_c4, d13CO2=constant_d13CO2, D14CO2=constant_D14CO2
+        )
+        isotope_c4.summarize()
 
-    comp.summarize()
+        # Calculate the expected isotopic patterns in locations given the competition model
+        comp.estimate_isotopic_discrimination(
+            d13CO2=constant_d13CO2,
+            Delta13C_C3_alone=isotope_c3.Delta13C,
+            Delta13C_C4_alone=isotope_c4.Delta13C,
+        )
 
-    # Profiling the isotopes submodule
-    # Create some entirely constant atmospheric isotope ratios
-    constant_d13CO2 = np.array([-8.4])
-    constant_D14CO2 = np.array([19.2])
+        comp.summarize()
 
-    # Calculate for the C3 model
-    isotope_c3 = CalcCarbonIsotopes(
-        pmod_c3, d13CO2=constant_d13CO2, D14CO2=constant_D14CO2
-    )
-    isotope_c3.summarize()
 
-    # Calculate for the C4 model
-    isotope_c4 = CalcCarbonIsotopes(
-        pmod_c4, d13CO2=constant_d13CO2, D14CO2=constant_D14CO2
-    )
-    isotope_c4.summarize()
 
-    # Calculate the expected isotopic patterns in locations given the competition model
-    comp.estimate_isotopic_discrimination(
-        d13CO2=constant_d13CO2,
-        Delta13C_C3_alone=isotope_c3.Delta13C,
-        Delta13C_C4_alone=isotope_c4.Delta13C,
-    )
+    def test_profile_subdaily(self, setup):
+        
+        from pyrealm.pmodel import (
+            FastSlowPModel,
+            FastSlowScaler
+        )
 
-    comp.summarize()
+        # Profiling the subdaily submodule
 
-    # Profiling the subdaily submodule
-
-    # FastSlowPModel with 1 hour noon acclimation window
-    fsscaler = FastSlowScaler(local_time)
-    fsscaler.set_window(
-        window_center=np.timedelta64(12, "h"),
-        half_width=np.timedelta64(1, "h"),
-    )
-    fs_pmod = FastSlowPModel(
-        env=pm_env,
-        fs_scaler=fsscaler,
-        handle_nan=True,
-        fapar=fapar,
-        ppfd=ppfd,
-        alpha=1 / 15,
-    )
+        # FastSlowPModel with 1 hour noon acclimation window
+        fsscaler = FastSlowScaler(self.local_time)
+        fsscaler.set_window(
+            window_center=np.timedelta64(12, "h"),
+            half_width=np.timedelta64(1, "h"),
+        )
+        fs_pmod = FastSlowPModel(
+            env=self.pm_env,
+            fs_scaler=fsscaler,
+            handle_nan=True,
+            fapar=self.fapar,
+            ppfd=self.ppfd,
+            alpha=1 / 15,
+        )
