ff10528fea199379b0ea09e13d4f4c48329029cc
tztsai <13818704679@163.com>
2023-12-04 17:56:49 +0000

    debug matplotlib dependency

--- profiling/report.py
+++ profiling/report.py
@@ -5,14 +5,16 @@ import re
 from io import StringIO
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import pandas as pd
+
+# import matplotlib.pyplot as plt
 
 # Read the profiling results
 root = Path.cwd()
 sio = StringIO()
 p = pstats.Stats(str(root / "prof/combined.prof"), stream=sio)
 p.print_stats(str(root))
+# p.print_callers(str(root))
 report = sio.getvalue()
 
 # Convert to a DataFrame and save to CSV
@@ -29,10 +31,11 @@ df[["filename", "lineno", "function"]] = df.pop(
 df.to_csv(root / "profiling/prof-report.csv", index=False)
 
 # Filter and plot the results
-df = df.query("tottime > 0.001")
-df["tag"] = df["filename"].str.extract(r"(\w+).py", expand=False) + "." + df["function"]
-df.plot.barh(y=["tottime", "cumtime"], x="tag", figsize=(20, 10))
-plt.ylabel("")
-plt.gca().invert_yaxis()
-plt.tight_layout()
-plt.savefig(root / "profiling/prof-report.png")
+# df = df.query("tottime > 0.001")
+# df["tag"] = df["filename"].str.extract(r"(\w+).py", expand=False) + "."
+# + df["function"]
+# df.plot.barh(y=["tottime", "cumtime"], x="tag", figsize=(20, 10))
+# plt.ylabel("")
+# plt.gca().invert_yaxis()
+# plt.tight_layout()
+# plt.savefig(root / "profiling/prof-report.png")
