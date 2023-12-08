39a72e4bfc7ee175264864637d31d26f9366803b
tztsai <1213815284@qq.com>
2023-12-08 12:31:29 +0000

    update CI and profiling report script

--- profiling/report.py
+++ profiling/report.py
@@ -4,16 +4,25 @@ import datetime
 import os
 import pstats
 import re
+from argparse import ArgumentParser
 from io import StringIO
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
+parser = ArgumentParser()
+parser.add_argument("--event", help="Github event name", default="none")
+parser.add_argument("--prof-dir", help="Profiling results directory", default="prof")
+parser.add_argument(
+    "--max-benchmark", help="Maximum number of benchmarks to plot", type=int, default=5
+)
+args = parser.parse_args()
+
 root = Path.cwd()
-orig_prof_path = root / "prof/combined.prof"
+orig_prof_path = root / args.prof_dir / "combined.prof"
+orig_graph_path = root / args.prof_dir / "combined.svg"
 prof_path = root / "profiling/profiling.prof"
-orig_graph_path = root / "prof/combined.svg"
 graph_path = root / "profiling/call-graph.svg"
 
 # Copy the profiling results to the current folder
@@ -49,6 +58,7 @@ df[["filename", "lineno", "function"]] = df.pop(
 dt = datetime.datetime.fromtimestamp(prof_path.stat().st_mtime)
 df.index = pd.Series([dt.strftime("%Y-%m-%d %H:%M:%S")] * len(df), name="timestamp")
 df["label"] = df["filename"].str.extract(r"(\w+).py").squeeze() + "." + df["function"]
+df["event"] = [args.event] * len(df)
 report_path = root / "profiling/prof-report.csv"
 df.to_csv(report_path, mode="a", header=not report_path.exists())
 
@@ -66,6 +76,7 @@ kpis = ["tottime_percall", "cumtime_percall", "tottime", "cumtime"]
 labels = df[kpis].idxmax()
 bm = df.pivot_table(index="timestamp", columns="label", values=kpis)
 bm = bm[list(map(tuple, labels.reset_index().values))]
+bm = bm.iloc[-args.max_benchmark:]  # fmt: skip
 bm.columns = bm.columns.map(lambda x: f"{x[1]}({x[0]})")
 bm.T.plot.barh(figsize=(20, 10))
 plt.tight_layout()
