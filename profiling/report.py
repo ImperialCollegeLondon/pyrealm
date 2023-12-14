24e29541b63bbd14010904b7948a8367ef48dce2
tztsai <1213815284@qq.com>
2023-12-14 11:21:21 +0000

    update report.py and CI

--- profiling/report.py
+++ profiling/report.py
@@ -87,5 +87,5 @@ plt.savefig(root / "profiling/benchmark.png")
 time_costs = bm.max(axis=1)
 t_ratio = time_costs / time_costs.shift(1) - 1
 latest_change = t_ratio.iloc[-1]
-if latest_change > 0:
+if latest_change > 1.05:
     exit(1)
