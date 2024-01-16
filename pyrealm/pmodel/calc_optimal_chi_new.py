539d245741ae7f2a5ca91c90789e31ed9b86012b
David Orme <davidorme@users.noreply.github.com>
2024-01-16 17:40:00 +0000

    Possibly slightly cleaner setting of class attributes

--- pyrealm/pmodel/calc_optimal_chi_new.py
+++ pyrealm/pmodel/calc_optimal_chi_new.py
@@ -12,6 +12,7 @@ from warnings import warn
 import numpy as np
 from numpy.typing import NDArray
 
+from pyrealm import ExperimentalFeatureWarning
 from pyrealm.constants import PModelConst
 from pyrealm.core.utilities import check_input_shapes, summarize_attrs
 from pyrealm.pmodel.pmodel_environment import PModelEnvironment
@@ -179,13 +180,18 @@ class NewCalcOptimalChi(ABC):
         summarize_attrs(self, attrs, dp=dp)
 
     @classmethod
-    def __init_subclass__(cls) -> None:
+    def __init_subclass__(cls, method: str, is_c4: bool) -> None:
         """Initialise a subclass deriving from this ABC."""
 
+        cls.method = method
+        cls.is_c4 = is_c4
         OPTIMAL_CHI_CLASS_REGISTRY[cls.method] = cls
 
 
-class OptimalChiPrentice14(NewCalcOptimalChi):
+# TODO - rootzonestress handling - new methods? Move rzs into env?
+
+
+class OptimalChiPrentice14(NewCalcOptimalChi, method="prentice14", is_c4=False):
     r"""Calculate :math:`\chi` for C3 plants following :cite:`Prentice:2014bc`.
 
     Optimal :math:`\chi` is calculated following Equation 8 in
@@ -236,9 +242,6 @@ class OptimalChiPrentice14(NewCalcOptimalChi):
         array([2.13211])
     """
 
-    method = "prentice14"
-    is_c4 = False
-
     def set_beta(self) -> None:
         """Set ``beta`` to a constant C3 specific value."""
         # leaf-internal-to-ambient CO2 partial pressure (ci/ca) ratio
@@ -267,7 +270,7 @@ class OptimalChiPrentice14(NewCalcOptimalChi):
         self.mjoc = self.mj / self.mc
 
 
-class OptimalChiC4(NewCalcOptimalChi):
+class OptimalChiC4(NewCalcOptimalChi, method="c4", is_c4=True):
     r"""Estimate :math:`\chi` for C4 plants following :cite:`Prentice:2014bc`.
 
     Optimal :math:`\chi` is calculated as in
@@ -292,9 +295,6 @@ class OptimalChiC4(NewCalcOptimalChi):
         >>> vals.mc.round(1)
         array([1.])
     """
-
-    method = "c4"
-    is_c4 = True
 
     def set_beta(self) -> None:
         """Set ``beta`` to a constant C4 specific value."""
@@ -324,3 +324,250 @@ class OptimalChiC4(NewCalcOptimalChi):
         self.mc = np.ones(self.shape)
         self.mj = np.ones(self.shape)
         self.mjoc = np.ones(self.shape)
+
+
+class OptimalChiLavergne20C3(NewCalcOptimalChi, method="lavergne20_c3", is_c4=False):
+    r"""Estimate :math:`\chi` for C3 plants using soil moisture corrected :math:`\beta`.
+
+    Optimal :math:`\chi` is calculated using a definition of the unit cost ratio $\beta$
+    as a function of soil moisture ($\theta$, m3 m-3), following :cite:`lavergne:2020a`:
+
+        .. math:: :nowrap:
+
+        \[
+            \beta = e ^ {b\theta + a},
+        \]
+
+    The coefficients are experimentally derived values with defaults taken from
+    Figure 6a of :cite:`lavergne:2020a` (:math:`a`,
+    :attr:`~pyrealm.constants.pmodel_const.PModelConst.lavergne_2020_a_c3`;
+    :math:`b`,
+    :attr:`~pyrealm.constants.pmodel_const.PModelConst.lavergne_2020_b_c3`).
+
+    Values of :math:`\chi` and other predictions are then calculated as in
+    :meth:`~pyrealm.pmodel.calc_optimal_chi.CalcOptimalChi.prentice14`. This method
+    requires that `env` includes estimates of :math:`\theta` and  is incompatible
+    with the `rootzonestress` approach.
+
+    Examples:
+        >>> import numpy as np
+        >>> env = PModelEnvironment(
+        ...     tc=np.array([20]), vpd=np.array([1000]), co2=np.array([400]),
+        ...     patm=np.array([101325.0]), theta=np.array([0.5])
+        ... )
+        >>> vals = OptimalChiLavergne20C3(env=env)
+        >>> vals.beta.round(5)
+        array([224.75255])
+        >>> vals.chi.round(5)
+        array([0.73663])
+        >>> vals.mc.round(5)
+        array([0.34911])
+        >>> vals.mj.round(5)
+        array([0.7258])
+        >>> vals.mjoc.round(5)
+        array([2.07901])
+    """
+
+    def set_beta(self) -> None:
+        """Set ``beta`` with soil moisture corrections."""
+
+        if self.env.theta is None:
+            raise RuntimeError(
+                "`OptimalChiLavergne20C3` requires soil moisture "
+                "in the PModelEnvironment"
+            )
+
+        # Calculate beta as a function of theta
+        self.beta = np.exp(
+            self.pmodel_const.lavergne_2020_b_c3 * self.env.theta
+            + self.pmodel_const.lavergne_2020_a_c3
+        )
+
+    def estimate_chi(self, xi_values: Optional[NDArray] = None) -> None:
+        """Estimate ``chi`` for C3 plants."""
+
+        if xi_values is not None:
+            _ = check_input_shapes(self.env.ca, xi_values)
+            self.xi = xi_values
+        else:
+            self.xi = np.sqrt(
+                (self.beta * (self.env.kmm + self.env.gammastar))
+                / (1.6 * self.env.ns_star)
+            )
+
+        self.chi = self.env.gammastar / self.env.ca + (
+            1.0 - self.env.gammastar / self.env.ca
+        ) * self.xi / (self.xi + np.sqrt(self.env.vpd))
+
+        # Calculate m and mc and m/mc
+        self.ci = self.chi * self.env.ca
+        self.mj = (self.ci - self.env.gammastar) / (self.ci + 2 * self.env.gammastar)
+        self.mc = (self.ci - self.env.gammastar) / (self.ci + self.env.kmm)
+        self.mjoc = self.mj / self.mc
+
+
+class OptimalChiLavergne20C4(NewCalcOptimalChi, method="lavergne20_c4", is_c4=True):
+    r"""Calculate soil moisture corrected :math:`\chi` for C4 plants.
+
+    This method calculates :math:`\beta` as a function of soil moisture following
+    the equation described in the
+    :meth:`~pyrealm.pmodel.calc_optimal_chi.CalcOptimalChi.lavergne20_c3` method.
+    However,
+    the default coefficients of the moisture scaling from :cite:`lavergne:2020a` for
+    C3 plants are adjusted to match the theoretical expectation that :math:`\beta`
+    for C4 plants is nine times smaller than :math:`\beta` for C3 plants (see
+    :meth:`~pyrealm.pmodel.calc_optimal_chi_new.OptimalChiC4`): :math:`b`
+    (:attr:`~pyrealm.constants.pmodel_const.PModelConst.lavergne_2020_b_c4`) is
+    unchanged but
+    :math:`a_{C4} = a_{C3} - log(9)`
+    (:attr:`~pyrealm.constants.pmodel_const.PModelConst.lavergne_2020_a_c4`) .
+
+    Following the calculation of :math:`\beta`, this method then follows the
+    calculations described in
+    :meth:`~pyrealm.pmodel.calc_optimal_chi_new.OptimalChiC4NoGamma`
+    ::math:`m_j = 1.0`
+    because photorespiration is negligible, but :math:`m_c` and hence
+    :math:`m_{joc}` are calculated.
+
+    Note:
+
+    This is an **experimental approach**. The research underlying
+    :cite:`lavergne:2020a`, found **no relationship** between C4 :math:`\beta`
+    values and soil moisture in leaf gas exchange measurements.
+
+    Examples:
+        >>> import numpy as np
+        >>> env = PModelEnvironment(
+        ...     tc=np.array([20]), vpd=np.array([1000]), co2=np.array([400]),
+        ...     patm=np.array([101325.0]), theta=np.array([0.5])
+        ... )
+        >>> vals = OptimalChiLavergne20C4(env=env)
+        >>> vals.beta.round(5)
+        array([24.97251])
+        >>> vals.chi.round(5)
+        array([0.44432])
+        >>> vals.mc.round(5)
+        array([0.28091])
+        >>> vals.mj.round(5)
+        array([1.])
+        >>> vals.mjoc.round(5)
+        array([3.55989])
+    """
+
+    def __init__(
+        self,
+        env: PModelEnvironment,
+        rootzonestress: NDArray = np.array([1.0]),
+        pmodel_const: PModelConst = PModelConst(),
+    ) -> None:
+        """Add experimental warning to __init__."""
+        super().__init__(
+            env=env, rootzonestress=rootzonestress, pmodel_const=pmodel_const
+        )
+
+        # Warn that this is an experimental feature.
+        warn(
+            "The lavergne20_c4 method is experimental, see the method documentation",
+            ExperimentalFeatureWarning,
+        )
+
+    def set_beta(self) -> None:
+        """Set ``beta`` with soil moisture corrections."""
+
+        # This method needs theta
+        if self.env.theta is None:
+            raise RuntimeError(
+                "`OptimalChiLavergne20C4` requires soil moisture "
+                "in the PModelEnvironment"
+            )
+        # Calculate beta as a function of theta
+        self.beta = np.exp(
+            self.pmodel_const.lavergne_2020_b_c4 * self.env.theta
+            + self.pmodel_const.lavergne_2020_a_c4
+        )
+
+    def estimate_chi(self, xi_values: Optional[NDArray] = None) -> None:
+        """Estimate ``chi`` for C4 plants excluding photorespiration."""
+
+        # Calculate chi and xi as in Prentice 14 but removing gamma terms.
+        self.xi = np.sqrt((self.beta * self.env.kmm) / (1.6 * self.env.ns_star))
+        self.chi = self.xi / (self.xi + np.sqrt(self.env.vpd))
+
+        # mj is equal to 1 as gammastar is null
+        self.mj = np.ones(self.shape)
+
+        # Calculate m and mc and m/mc
+        self.ci = self.chi * self.env.ca
+        self.mc = (self.ci) / (self.ci + self.env.kmm)
+        self.mjoc = self.mj / self.mc
+
+
+class OptimalChiC4NoGamma(NewCalcOptimalChi, method="c4_no_gamma", is_c4=True):
+    r"""Calculate optimal chi for C4 plants assuming negligible photorespiration.
+
+    Calculates :math:`\chi` assuming that photorespiration (:math:`\Gamma^\ast`) is
+    negligible for C4 plants. This simplifies the calculation of :math:`\xi` and
+    :math:`\chi` compared to :meth:`~pyrealm.pmodel.calc_optimal_chi.CalcOptimalChi.c4`,
+    but uses the same C4 specific estimate of the unit cost ratio :math:`\beta`,
+    :attr:`~pyrealm.constants.pmodel_const.PModelConst.beta_cost_ratio_c4`.
+
+        .. math:: :nowrap:
+
+        \[
+            \begin{align*}
+                \chi &= \xi / (\xi + \sqrt D ), \text{where}\\ \xi &= \sqrt{(\beta
+                K) / (1.6 \eta^{*}))}
+            \end{align*}
+        \]
+
+    In addition, :math:`m_j = 1.0`  because photorespiration is negligible in C4
+    photosynthesis, but :math:`m_c` and hence :math:`m_{joc}` are calculated, not
+    set to one.
+
+        .. math:: :nowrap:
+
+        \[
+            \begin{align*}
+                m_c &= \dfrac{\chi}{\chi + \frac{K}{c_a}}
+            \end{align*}
+        \]
+
+    Examples:
+        >>> import numpy as np
+        >>> env = PModelEnvironment(
+        ...     tc=np.array([20]), vpd=np.array([1000]),
+        ...     co2=np.array([400]), patm=np.array([101325.0])
+        ... )
+        >>> vals = OptimalChiC4NoGamma(env=env)
+        >>> vals.chi.round(5)
+        array([0.3919])
+        >>> vals.mj.round(1)
+        array([1.])
+        >>> vals.mc.round(1)
+        array([0.3])
+    """
+
+    def set_beta(self) -> None:
+        """Set constant ``beta`` for C4 plants."""
+
+        # Calculate chi and xi as in Prentice 14 but removing gamma terms.
+        self.beta = self.pmodel_const.beta_cost_ratio_c4
+
+    def estimate_chi(self, xi_values: Optional[NDArray] = None) -> None:
+        """Estimate ``chi`` for C4 plants excluding photorespiration."""
+
+        self.xi = np.sqrt((self.beta * self.env.kmm) / (1.6 * self.env.ns_star))
+        # self.xi = np.sqrt(
+        #     (self.beta * self.rootzonestress * self.env.kmm) /
+        #     (1.6 * self.env.ns_star)
+        # )
+
+        self.chi = self.xi / (self.xi + np.sqrt(self.env.vpd))
+
+        # mj is equal to 1 as gammastar is null
+        self.mj = np.ones(self.shape)
+
+        # Calculate m and mc and m/mc
+        self.ci = self.chi * self.env.ca
+        self.mc = (self.ci) / (self.ci + self.env.kmm)
+        self.mjoc = self.mj / self.mc
