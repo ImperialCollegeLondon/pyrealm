7f4b0ca879cdcde9cca6580a9106bad5af91d1e0
Surbhi Goel <surbhigoel77@github.com>
2024-02-15 13:35:45 +0000

    Added updated test for new OptimalChi

--- tests/unit/pmodel/test_calc_optimal_chi.py
+++ tests/unit/pmodel/test_calc_optimal_chi.py
@@ -1,113 +1,73 @@
 """Testing CalcOptimal submodule."""
 
+
 import numpy as np
 import pytest
 
-from pyrealm.constants import PModelConst
-from pyrealm.pmodel import CalcOptimalChi, PModelEnvironment
+from pyrealm.pmodel.optimal_chi import (
+    OptimalChiC4,
+    OptimalChiC4NoGamma,
+    OptimalChiC4RootzoneStress,
+    OptimalChiLavergne20C3,
+    OptimalChiLavergne20C4,
+    OptimalChiPrentice14,
+    OptimalChiPrentice14RootzoneStress,
+)
+from pyrealm.pmodel.pmodel_environment import PModelEnvironment
 
 
 @pytest.fixture
-def rootzonestress():
-    """Return the root zone stress."""
-    return np.array([1])
-    # return np.array([0])
-
-
-@pytest.fixture
-def const():
-    """Return the PModelConst."""
-    return PModelConst()
+def photo_env():
+    """Photosynthesis Environment setup."""
+    return PModelEnvironment(
+        tc=np.array([20]),
+        vpd=np.array([1000]),
+        co2=np.array([400]),
+        patm=np.array([101325.0]),
+        rootzonestress=np.array([1]),
+        theta=np.array([0.5]),
+    )
 
 
 @pytest.mark.parametrize(
-    argnames=["pmodelenv_args", "pmodel_args", "expected"],
-    argvalues=[
-        (  # Single site, C3 prentice14
-            dict(tc=20, patm=101325, co2=400, vpd=1000),
-            dict(method_optchi="prentice14"),
-            dict(
-                # beta=24.97251,
-                chi=0.69435,
-                mc=0.33408,
-                mj=0.7123,
-                mjoc=2.13211,
-            ),
-        ),
-        (  # Single site, C3 lavergne20
-            dict(tc=20, patm=101325, co2=400, vpd=1000, theta=0.5),
-            dict(method_optchi="lavergne20_c3"),
-            dict(
-                beta=224.75255,
-                chi=0.73663,
-                mc=0.34911,
-                mj=0.7258,
-                mjoc=2.07901,
-            ),
-        ),
-        (  # Single site, C4
-            dict(tc=20, patm=101325, co2=400, vpd=1000),
-            dict(method_optchi="c4"),
-            dict(
-                # beta=21.481,
-                chi=0.44967,
-                mc=1.0,
-                mj=1.0,
-                # mjoc=-19.619,
-            ),
-        ),
-        # (  # Single site, C4 no gamma
-        #     dict(tc=20, patm=101325, co2=400, vpd=1000),
-        #     dict(method_optchi="c4_no_gamma"),
-        #     dict(
-        #         # beta=21.481,
-        #         chi=0.3919,
-        #         mc=0.3,
-        #         mj=1.0,
-        #         # mjoc=-19.619,
-        #     ),
-        # ),
-        (  # Single site, C4 lavergne20
-            dict(tc=20, patm=101325, co2=400, vpd=1000, theta=0.5),
-            dict(method_optchi="lavergne20_c4"),
-            dict(
-                beta=24.97251,
-                chi=0.44432,
-                mc=0.28091,
-                mj=1,
-                mjoc=3.55989,
-            ),
-        ),
-        # (  # Single site, C3 prentice14 with all-zero inputs
-        #     dict(tc=20, patm=101325, co2=400, vpd=1000),
-        #     dict(method_optchi="prentice14"),
-        #     dict(
-        #         # beta=24.97251,
-        #         chi=0,
-        #         mc=0,
-        #         mj=0,
-        #         mjoc=0,
-        #     ),
-        # ),
-        # (  # Single site, C3 prentice14 with all-zero inputs
-        #     dict(tc=20, patm=101325, co2=400, vpd=1000),
-        #     dict(method_optchi="prentice14"),
-        #     dict(
-        #         # beta=24.97251,
-        #         chi=np.nan,
-        #         mc=np.nan,
-        #         mj=np.nan,
-        #         mjoc=np.nan,
-        #     ),
-        # ),
+    """optimal_chi_class""",
+    [
+        OptimalChiPrentice14,
+        OptimalChiPrentice14RootzoneStress,
+        OptimalChiC4,
+        OptimalChiC4RootzoneStress,
+        OptimalChiLavergne20C3,
+        OptimalChiLavergne20C4,
+        OptimalChiC4NoGamma,
     ],
 )
-def test_calcoptimalchi(pmodelenv_args, pmodel_args, expected, rootzonestress, const):
-    """Test CalcOptimalChi."""
-    env = PModelEnvironment(**pmodelenv_args)
-    calc_optimal_chi = CalcOptimalChi(
-        env, rootzonestress, pmodel_args["method_optchi"], const
-    )
+def test_set_beta(optimal_chi_class, photo_env):
+    """Test that beta is set correctly."""
+    optimal_chi_instance = optimal_chi_class(env=photo_env)
+    optimal_chi_instance.set_beta()
+    # Test that beta attribute is set correctly
+    assert isinstance(optimal_chi_instance.beta, np.ndarray)
 
-    for key in expected:
-        assert np.allclose(getattr(calc_optimal_chi, key), expected[key], atol=0.0001)
+
+@pytest.mark.parametrize(
+    """optimal_chi_class""",
+    [
+        OptimalChiPrentice14,
+        OptimalChiPrentice14RootzoneStress,
+        OptimalChiC4,
+        OptimalChiC4RootzoneStress,
+        OptimalChiLavergne20C3,
+        OptimalChiLavergne20C4,
+        OptimalChiC4NoGamma,
+    ],
+)
+def test_estimate_chi(optimal_chi_class, photo_env):
+    """Test that chi is estimated correctly."""
+    optimal_chi_instance = optimal_chi_class(env=photo_env)
+    optimal_chi_instance.set_beta()
+    optimal_chi_instance.estimate_chi()
+    # Test that chi and other related attributes are calculated correctly
+    assert isinstance(optimal_chi_instance.chi, np.ndarray)
+    assert isinstance(optimal_chi_instance.mc, np.ndarray)
+    assert isinstance(optimal_chi_instance.mj, np.ndarray)
+    assert isinstance(optimal_chi_instance.mjoc, np.ndarray)
