4869a0a47d27deab5defb93aada237bb4699fd34
Tianzhang Cai <1213815284@qq.com>
2024-03-18 15:05:02 +0000

    Update tests/unit/core/test_evaluate_horner_polynomial.py
    Co-authored-by: David Orme <davidorme@users.noreply.github.com>

--- tests/unit/core/test_evaluate_horner_polynomial.py
+++ tests/unit/core/test_evaluate_horner_polynomial.py
@@ -6,7 +6,11 @@ from pyrealm.core.utilities import evaluate_horner_polynomial
 
 
 def test_evaluate_horner_polynomial():
-    """Test the evaluate_horner_polynomial function."""
+    """Test the evaluate_horner_polynomial function.
+    
+    This checks that the normal form (a + bx + cx2 + dx3) and output from the
+    horner form evaluation (a + x(b + x(c + dx))) are equivalent for powers up to 10.
+    """
     for deg in range(10):
         cf = np.random.normal(deg + 1)
         x = np.random.normal()
