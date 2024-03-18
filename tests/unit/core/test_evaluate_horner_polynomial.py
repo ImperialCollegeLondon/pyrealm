de8eca4a9560da49a0c8da45833694466d83b42e
Tianzhang Cai <1213815284@qq.com>
2024-03-18 15:04:46 +0000

    Update tests/unit/core/test_evaluate_horner_polynomial.py
    Co-authored-by: David Orme <davidorme@users.noreply.github.com>

--- tests/unit/core/test_evaluate_horner_polynomial.py
+++ tests/unit/core/test_evaluate_horner_polynomial.py
@@ -8,8 +8,8 @@ from pyrealm.core.utilities import evaluate_horner_polynomial
 def test_evaluate_horner_polynomial():
     """Test the evaluate_horner_polynomial function."""
     for deg in range(10):
-        cf = np.random.rand(deg + 1)
-        x = np.random.rand()
+        cf = np.random.normal(deg + 1)
+        x = np.random.normal()
         normal = np.sum(cf * np.power(x, range(len(cf))))
         horner = evaluate_horner_polynomial(x, cf)
         assert np.allclose(normal, horner)
