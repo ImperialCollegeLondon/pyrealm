504b5c812718063e00e03cabc61d57aad5fd2ca9
David Orme <davidorme@users.noreply.github.com>
2024-03-26 09:57:03 +0000

    Fixing horner test and expanding inputs

--- tests/unit/core/test_evaluate_horner_polynomial.py
+++ tests/unit/core/test_evaluate_horner_polynomial.py
@@ -1,19 +1,30 @@
 """Test of the evaluate_horner_polynomial function."""
 
 import numpy as np
+import pytest
 
-from pyrealm.core.utilities import evaluate_horner_polynomial
 
-
-def test_evaluate_horner_polynomial():
+@pytest.mark.parametrize(
+    argnames="inputs",
+    argvalues=[
+        pytest.param(5.5, id="float"),
+        pytest.param(np.array(5.5), id="0D array"),
+        pytest.param(np.random.normal(size=10), id="1D array"),
+        pytest.param(np.random.normal(size=(10, 10)), id="2D array"),
+    ],
+)
+def test_evaluate_horner_polynomial(inputs):
     """Test the evaluate_horner_polynomial function.
 
     This checks that the normal form (a + bx + cx2 + dx3) and output from the
-    horner form evaluation (a + x(b + x(c + dx))) are equivalent for powers up to 10.
+    horner form evaluation (a + x(b + x(c + dx))) are equivalent for powers up to 10,
+    for a range of different inputs.
     """
+
+    from pyrealm.core.utilities import evaluate_horner_polynomial
+
     for deg in range(10):
-        cf = np.random.normal(deg + 1)
-        x = np.random.normal()
-        normal = np.sum(cf * np.power(x, range(len(cf))))
-        horner = evaluate_horner_polynomial(x, cf)
+        cf = np.random.normal(size=deg + 1)
+        normal = np.polynomial.polynomial.polyval(inputs, cf)
+        horner = evaluate_horner_polynomial(inputs, cf)
         assert np.allclose(normal, horner)
