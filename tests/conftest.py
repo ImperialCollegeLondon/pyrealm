ed90304039f1795a589120f524fb830e95bbb292
David Orme <davidorme@users.noreply.github.com>
2024-04-17 14:10:38 +0100

    Attempt to balance peak load at about 12GB

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,5 @@
 
 def pytest_addoption(parser):
     """Allow profiling dataset sizes to be scaled from the command line."""
-    parser.addoption("--pmodel-profile-scaleup", type=int, default=8)
-    parser.addoption("--splash-profile-scaleup", type=int, default=200)
+    parser.addoption("--pmodel-profile-scaleup", type=int, default=6)
+    parser.addoption("--splash-profile-scaleup", type=int, default=125)
