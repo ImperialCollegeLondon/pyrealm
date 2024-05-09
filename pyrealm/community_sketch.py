b1467c26ad35d5494fc42a3209d26f417cb474fb
Amy Pike <amymaypike@hotmail.co.uk>
2024-05-09 17:53:09 +0100

    partial very sketchy implementation for canopy model

--- pyrealm/community_sketch.py
+++ pyrealm/community_sketch.py
@@ -1,25 +1,101 @@
 """A very incomplete sketch of some community and demography functionality."""
 
+from dataclasses import dataclass
+
 import numpy as np
-import pandas as pd
 from numpy.typing import NDArray
 
 
-class Flora:
-    """Beginnings of a container class for PFTs."""
+@dataclass
+class PlantFunctionalType:
+    """placeholder."""
 
-    def __init__(self, pft_data: pd.DataFrame):
-        # TODO:
-        # - validate the incoming data against the expectations of the T model
-        #   parameterisation.
-        # - Where are those defaults now defined? Just use TModelTraits as a defaults
-        #   argument constructor? Provides expected fields and defaults.
-        # - support automatic default values?
-        # - add canopy shape parameters m and n and internally calculate derived
-        #   constant q_m for canopy calculations.
+    name: str
+    a_hd: float
+    ca_ratio: float
+    h_max: float
+    lai: float
+    par_ext: float
+    resp_f: float
+    resp_r: float
+    resp_s: float
+    rho_s: float
+    sla: float
+    tau_f: float
+    tau_r: float
+    yld: float
+    zeta: float
+    m: float
+    n: float
 
-        self.pft_data = pft_data
-        self.pft_names = set(pft_data["pft"])
+
+@dataclass
+class CohortSpatialDistribution(dict[str, int]):
+    """placeholder."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        # TODO keyed dictionary of cell IDs and number
+        #  of members in a given cell - e.g. cell_number_1 : 6
+
+
+@dataclass
+class Cohort:
+    """placeholder."""
+
+    dbh: float
+    pft: str
+    spatial_distribution: CohortSpatialDistribution
+
+
+class Flora(dict[str, PlantFunctionalType]):
+    """Defines the flora used in a ``virtual_ecosystem`` model.
+
+    The flora is the set of plant functional types used within a particular simulation
+    and this class provides dictionary-like access to a defined set of
+    :class:`~virtual_ecosystem.models.plants.functional_types.PlantFunctionalType`
+    instances.
+
+    Instances of this class should not be altered during model fitting, at least until
+    the point where plant evolution is included in the modelling process.
+
+    Args:
+        pfts: A list of ``PlantFunctionalType`` instances, which must not have
+            duplicated
+            :attr:`~virtual_ecosystem.models.plants.functional_types.PlantFunctionalType.pft_name`
+            attributes.
+    """
+
+    def __init__(self, pfts: list[PlantFunctionalType]) -> None:
+        # Get the names and check there are no duplicates
+        super().__init__()
+        pft_names = [p.name for p in pfts]
+        if len(pft_names) != len(set(pft_names)):
+            msg = "Duplicated plant functional type names in creating Flora instance."
+            raise ValueError(msg)
+
+        for name, pft in zip(pft_names, pfts):
+            self[name] = pft
+
+
+@dataclass
+class CohortGeometry:
+    """placeholder."""
+
+    cohort: Cohort
+    height: float
+    crown_area: float
+    crown_fraction: float
+    mass_stm: float
+    mass_fol: float
+    mass_swd: float
+
+
+class CommunityGeometry(list[CohortGeometry]):
+    """placeholder."""
+
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Community:
@@ -28,19 +104,15 @@ class Community:
     This could handle multiple locations or a single location.
     """
 
-    def __init__(self, flora: Flora, inventory: pd.DataFrame) -> None:
+    def __init__(self, flora: Flora, cohorts: list[Cohort]) -> None:
         self.flora: Flora = flora
-
-        # TODO - validate the inventory data (field names and values)
-        self.inventory = inventory
+        self.cohorts: list[Cohort] = cohorts
 
         # Things populated by methods
         self.canopy_layer_heights: NDArray[np.float32]
         self.lai_by_canopy_layer: NDArray[np.float32]
 
         self.calculate_geometry()
-        self.calculate_canopy()
-
         # Things to add later
 
         # pft keyed dictionary of propagule density, size, energy content pft keyed
@@ -61,73 +133,53 @@ class Community:
 
         pass
 
-    def calculate_geometry(self) -> None:
-        """Calculates the geometry and mass data for each cohort."""
+    def calculate_geometry(self) -> list[CohortGeometry]:
+        """placeholder."""
+        results = []
 
-        # NOTE - Don't know if this makes a difference, trying to avoid many
-        #        getattr(self.data, 'xyz') calls by using a local reference.
-        inventory = self.inventory
+        for cohort in self.cohorts:
+            pft = self.flora.get(cohort.pft)
 
-        # Broadcast the PFT trait values onto the matching rows in the inventory data
-        # TODO: handle NAs following merge or just rely on robust initial validation
-        #       that all inventory pfts exist in flora.pft_names
-        # NOTE: is it better to avoid repeated merge operations by making the inventory
-        #       include the trait data at __init__ and when cohorts are added. Cost of
-        #       increasing data storage and repeated data, but probably faster.
-        traits = inventory[["pft"]].merge(self.flora.pft_data, how="left")
+            # don't think this is an elegant way to do this, but putting it here for now
+            if pft is None:
+                raise Exception("PFT not provided in list")
 
-        # Height of tree from diameter, Equation (4) of Li ea.
-        inventory["height"] = traits["h_max"] * (
-            1 - np.exp(-traits["a_hd"] * inventory["dbh"] / traits["h_max"])
-        )
+            height = pft.h_max * (1 - np.exp(-pft.a_hd) * cohort.dbh / pft.h_max)
 
-        # Crown area of tree, Equation (8) of Li ea.
-        inventory["crown_area"] = (
-            ((np.pi * traits["ca_ratio"]) / (4 * traits["a_hd"]))
-            * inventory["dbh"]
-            * inventory["height"]
-        )
+            # Crown area of tree, Equation (8) of Li ea.
+            crown_area = (np.pi * pft.ca_ratio / (4 * pft.a_hd)) * cohort.dbh * height
 
-        # Crown fraction, Equation (11) of Li ea.
-        inventory["crown_fraction"] = inventory["height"] / (
-            traits["a_hd"] * inventory["dbh"]
-        )
+            # Crown fraction, Equation (11) of Li ea.
+            crown_fraction = height / (pft.a_hd * cohort.dbh)
 
-        # Masses
-        inventory["mass_stm"] = (
-            (np.pi / 8)
-            * (inventory["dbh"] ** 2)
-            * inventory["height"]
-            * traits["rho_s"]
-        )
-        inventory["mass_fol"] = (
-            inventory["crown_area"] * traits["lai"] * (1 / traits["sla"])
-        )
-        inventory["mass_swd"] = (
-            inventory["crown_area"]
-            * traits["rho_s"]
-            * inventory["height"]
-            * (1 - inventory["crown_fraction"] / 2)
-            / traits["ca_ratio"]
-        )
+            # Masses
+            mass_stem = (np.pi / 8) * (cohort.dbh**2) * height * pft.rho_s
+            mass_fol = crown_area * pft.lai * (1 / pft.sla)
+            mass_swd = (
+                crown_area
+                * pft.rho_s
+                * height
+                * (1 - crown_fraction / 2)
+                / pft.ca_ratio
+            )
 
-        # TODO: Use canopy constants (m, n) to calculate
+            results.append(
+                CohortGeometry(
+                    cohort,
+                    height,
+                    crown_area,
+                    crown_fraction,
+                    mass_stem,
+                    mass_fol,
+                    mass_swd,
+                )
+            )
 
-        # Update stored inventory with added geometry
-        self.inventory = inventory
-
-    def calculate_canopy(self) -> None:
-        """Uses the canopy model to get the canopy structure.
-
-        If the class handles multiple communities, this needs to loop over each location
-        and it is probably easier not to.
-
-        Sets self.layer_heights and a per cohort area of leaf area within each layer.
-        """
-
-        # TODO:
-        # - implement canopy calculation
-        pass
+            # feel like there should be a more lightweight way to store this than
+            # storing the cohorts as nested objects, with spatial distribution as
+            # a nested object in that, maybe a dict of cohorts and spatial
+            # distribution, and a dict of cohort geometry
+        return results
 
     def calculate_gpp(self, cell_ppfd: NDArray, lue: NDArray) -> None:
         """Estimate the gross primary productivity.
@@ -136,4 +188,47 @@ class Community:
 
         """
 
+        pass
+
+
+def calculate_qm(m: float, n: float) -> float:
+    """placeholder."""
+    return (
+        m
+        * n
+        * ((n - 1) / (m * n - 1)) ** (1 - 1 / n)
+        * (((m - 1) * n) / (m * n - 1)) ** (m - 1)
+    )
+
+
+def calculate_stem_canopy_factors(
+    cohort_geometry: CohortGeometry, flora: Flora
+) -> tuple[int, int]:
+    """placeholder."""
+    pft = flora[cohort_geometry.cohort.pft]
+    # TODO assuming m and n are per plant functional type here
+    m = pft.m
+    n = pft.n
+    qm = calculate_qm(m, n)
+
+    # Height of maximum crown radius
+    zm = cohort_geometry.height * ((n - 1) / (m * n - 1)) ** (1 / n)
+
+    # Slope to give Ac at zm
+    r0 = 1 / qm * np.sqrt(cohort_geometry.crown_area / np.pi)
+
+    return zm, r0
+
+
+class Canopy:
+    """placeholder."""
+
+    def __init__(self) -> None:
+        """placeholder."""
+        self.canopy_layer_heights: object
+
+    def calculate_canopy_layer_heights(
+        self, community_geometry: CommunityGeometry
+    ) -> None:
+        """placeholder."""
         pass
