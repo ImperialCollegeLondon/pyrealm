6496f07d8ec4c12fbbbc859e7b19483c95a9d181
Amy Pike <amymaypike@hotmail.co.uk>
2024-05-21 17:04:57 +0100

    continuing implementation of canopy model

--- pyrealm/community_sketch.py
+++ pyrealm/community_sketch.py
@@ -1,12 +1,16 @@
 """A very incomplete sketch of some community and demography functionality."""
 
+import json
 from dataclasses import dataclass
 from typing import TypeAlias
 
 import numpy as np
+from dataclasses_json import dataclass_json
 from numpy.typing import NDArray
+from scipy.optimize import root_scalar
 
 
+@dataclass_json
 @dataclass
 class PlantFunctionalType:
     """placeholder."""
@@ -30,6 +34,12 @@ class PlantFunctionalType:
     n: float
 
 
+# TODO refactor these classes a bit, better naming for Cohort
+# and Cohort geometry.
+# One is for importing and one is augmented with height calculations
+
+
+@dataclass_json
 @dataclass
 class Cohort:
     """placeholder."""
@@ -37,6 +47,13 @@ class Cohort:
     pft_name: str
     dbh: float
     number_of_members: int
+
+
+@dataclass_json
+@dataclass
+class ImportedCommunity:
+    cell_id: int
+    cohorts: list[Cohort]
 
 
 class Flora(dict[str, PlantFunctionalType]):
@@ -71,10 +88,10 @@ class Flora(dict[str, PlantFunctionalType]):
 
 @dataclass
 class CohortGeometry:
-    """placeholder."""
+    """Cohort geometry calculated using the T Model."""
 
     number_of_members: int
-    pft_name: str
+    pft: PlantFunctionalType
     dbh: float
     height: float
     crown_area: float
@@ -82,9 +99,6 @@ class CohortGeometry:
     mass_stem: float
     mass_fol: float
     mass_swd: float
-
-
-CommunityGeometry: TypeAlias = list[CohortGeometry]
 
 
 class Community:
@@ -96,11 +110,9 @@ class Community:
     def __init__(self, flora: Flora, cohorts: list[Cohort]) -> None:
         self.flora: Flora = flora
         self.cohorts: list[Cohort] = cohorts
-        self.community_geometry: CommunityGeometry = self.calculate_geometry()
-
-        # Things populated by methods
-        self.canopy_layer_heights: NDArray[np.float32]
-        self.lai_by_canopy_layer: NDArray[np.float32]
+        self.cohort_geometries: list[CohortGeometry] = map(
+            self.calculate_cohort_geometry_using_t_model, self.cohorts
+        )
 
         # Things to add later
 
@@ -112,8 +124,52 @@ class Community:
         # per cohort structure of fruit mass, energy, size?
         self.fruit: object
 
-    def recruit(self) -> None:
-        """Add new cohorts from the seedbank."""
+    def calculate_cohort_geometry_using_t_model(self, cohort: Cohort) -> CohortGeometry:
+        """placeholder."""
+
+        pft = self.flora.get(cohort.pft_name)
+
+        # don't think this is an elegant way to do this, but putting it here for now
+        if pft is None:
+            raise Exception("PFT not provided in list")
+
+        height = pft.h_max * (1 - np.exp(-pft.a_hd) * cohort.dbh / pft.h_max)
+
+        # Crown area of tree, Equation (8) of Li ea.
+        crown_area = (np.pi * pft.ca_ratio / (4 * pft.a_hd)) * cohort.dbh * height
+
+        # Crown fraction, Equation (11) of Li ea.
+        crown_fraction = height / (pft.a_hd * cohort.dbh)
+
+        # Masses
+        mass_stem = (np.pi / 8) * (cohort.dbh**2) * height * pft.rho_s
+        mass_fol = crown_area * pft.lai * (1 / pft.sla)
+        mass_swd = (
+            crown_area * pft.rho_s * height * (1 - crown_fraction / 2) / pft.ca_ratio
+        )
+
+        return CohortGeometry(
+            cohort.number_of_members,
+            pft,
+            cohort.dbh,
+            height,
+            crown_area,
+            crown_fraction,
+            mass_stem,
+            mass_fol,
+            mass_swd,
+        )
+
+    def calculate_geometry_using_t_model(self) -> list[CohortGeometry]:
+        """placeholder."""
+        results = []
+
+        for cohort in self.cohorts:
+            results.append(self.calculate_cohort_geometry_using_t_model(cohort))
+        return results
+
+        def recruit(self) -> None:
+            """Add new cohorts from the seedbank."""
 
         pass
 
@@ -121,51 +177,6 @@ class Community:
         """Remove cohort data for empty cohorts."""
 
         pass
-
-    def calculate_geometry(self) -> list[CohortGeometry]:
-        """placeholder."""
-        results = []
-
-        for cohort in self.cohorts:
-            pft = self.flora.get(cohort.pft_name)
-
-            # don't think this is an elegant way to do this, but putting it here for now
-            if pft is None:
-                raise Exception("PFT not provided in list")
-
-            height = pft.h_max * (1 - np.exp(-pft.a_hd) * cohort.dbh / pft.h_max)
-
-            # Crown area of tree, Equation (8) of Li ea.
-            crown_area = (np.pi * pft.ca_ratio / (4 * pft.a_hd)) * cohort.dbh * height
-
-            # Crown fraction, Equation (11) of Li ea.
-            crown_fraction = height / (pft.a_hd * cohort.dbh)
-
-            # Masses
-            mass_stem = (np.pi / 8) * (cohort.dbh**2) * height * pft.rho_s
-            mass_fol = crown_area * pft.lai * (1 / pft.sla)
-            mass_swd = (
-                crown_area
-                * pft.rho_s
-                * height
-                * (1 - crown_fraction / 2)
-                / pft.ca_ratio
-            )
-
-            results.append(
-                CohortGeometry(
-                    cohort.number_of_members,
-                    cohort.pft_name,
-                    cohort.dbh,
-                    height,
-                    crown_area,
-                    crown_fraction,
-                    mass_stem,
-                    mass_fol,
-                    mass_swd,
-                )
-            )
-        return results
 
     def calculate_gpp(self, cell_ppfd: NDArray, lue: NDArray) -> None:
         """Estimate the gross primary productivity.
@@ -187,12 +198,10 @@ def calculate_q_m(m: float, n: float) -> float:
     )
 
 
-def calculate_stem_canopy_factors(
-    cohort_geometry: CohortGeometry, pft: PlantFunctionalType
-) -> tuple[int, int]:
+def calculate_stem_canopy_factors(cohort_geometry: CohortGeometry) -> tuple[int, int]:
     """placeholder."""
-    m = pft.m
-    n = pft.n
+    m = cohort_geometry.pft.m
+    n = cohort_geometry.pft.n
     q_m = calculate_q_m(m, n)
 
     # Height of maximum crown radius
@@ -205,9 +214,9 @@ def calculate_stem_canopy_factors(
     return z_m, r_0
 
 
-def calculate_relative_canopy_radius_at_z(
-    z: float, H: float, m: float, n: float
-) -> float:
+def calculate_relative_canopy_radius(
+    z: np.ndarray, H: float, m: float, n: float
+) -> np.ndarray:
     """Calculate q(z) at a given height, z."""
 
     z_over_H = z / H
@@ -215,29 +224,29 @@ def calculate_relative_canopy_radius_at_z(
     return m * n * z_over_H ** (n - 1) * (1 - z_over_H**n) ** (m - 1)
 
 
-calculate_relative_canopy_radius_profile = np.vectorize(
-    calculate_relative_canopy_radius_at_z
-)
+def create_z_axis(z_min: float, z_max: float, z_resolution: float = 0.05):
+    max_height_padding = 1
+    floating_point_correction = 0.00001
+
+    z = np.arange(z_min, z_max + max_height_padding, z_resolution)
+    z = np.sort(np.concatenate([z, z_max - floating_point_correction]))
+
+    return z
 
 
 def calculate_crown_radius_profile_for_cohort(
     cohort_geometry: CohortGeometry,
-    pft: PlantFunctionalType,
     z_resolution: float = 0.05,
 ) -> np.typing.NDArray:
     """Calculate the crown radius profile for a given cohort."""
 
-    max_height_padding = 1
-    floating_point_correction = 0.00001
+    z = create_z_axis(0, cohort_geometry.height, z_resolution)
 
-    z = np.arange(0, cohort_geometry.height + max_height_padding, z_resolution)
-    z = np.sort(np.concatenate([z, cohort_geometry.height - floating_point_correction]))
-
-    z_m, r_0 = calculate_stem_canopy_factors(cohort_geometry, pft)
+    z_m, r_0 = calculate_stem_canopy_factors(cohort_geometry)
 
     # calculate r(z) = r0 * q(z) for a cohort
-    r_z = r_0 * calculate_relative_canopy_radius_profile(
-        z, cohort_geometry.height, pft.m, pft.n
+    r_z = r_0 * calculate_relative_canopy_radius(
+        z, cohort_geometry.height, cohort_geometry.pft.m, cohort_geometry.pft.n
     )
 
     # When z > H, r_z < 0, so set radius to 0 where rz < 0
@@ -246,102 +255,39 @@ def calculate_crown_radius_profile_for_cohort(
     return r_z
 
 
-# def calculate_projected_canopy_area_profile_for_individual(
-#     z: np.ndarray, cohort_geometry: CohortGeometry, pft: PlantFunctionalType
-# ) -> int:
-#     """Calculate projected crown area above a given height.
-#
-#     This function takes PFT specific parameters (shape parameters) and stem specific
-#     sizes and estimates the projected crown area above a given height $z$. The inputs
-#     can either be scalars describing a single stem or arrays representing a community
-#     of stems. If only a single PFT is being modelled then `m`, `n`, `qm` and `fg` can
-#     be scalars with arrays `H`, `Ac` and `zm` giving the sizes of stems within that
-#     PFT.
-#
-#     Args:
-#         :param z: array of heights
-#         :param cohort_geometry: calculated geometry of cohort
-#         :param pft: plant functional type
-#     """
-#     q_m = calculate_q_m(pft.m, pft.n)
-#     z_m, r_0 = calculate_stem_canopy_factors(cohort_geometry, pft)
-#
-#     # Calculate q(z)
-#     q_z = calculate_relative_canopy_radius_profile(
-#         z, cohort_geometry.height, pft.m, pft.n
-#     )
-#
-#     # Calculate Ap given z > z_m
-#     A_p = cohort_geometry.crown_area * (q_z / q_m) ** 2
-#     # Set Ap = Ac where z <= z_m
-#     A_p = np.where(z <= z_m, cohort_geometry.crown_area, A_p)
-#     # Set Ap = 0 where z > H
-#     A_p = np.where(z > cohort_geometry.height, 0, A_p)
-#
-#     return A_p
+def calculate_projected_canopy_area_for_individual(
+    z: float, cohort_geometry: CohortGeometry
+) -> int:
+    """Calculate projected crown area above a given height.
 
+    This function takes PFT specific parameters (shape parameters) and stem specific
+    sizes and estimates the projected crown area above a given height $z$. The inputs
+    can either be scalars describing a single stem or arrays representing a community
+    of stems. If only a single PFT is being modelled then `m`, `n`, `qm` and `fg` can
+    be scalars with arrays `H`, `Ac` and `zm` giving the sizes of stems within that
+    PFT.
 
-# def solve_canopy_closure_height(
-#     z: float,
-#     l: int,
-#     A: float,
-#     fG: float,
-#     m: float,
-#     n: float,
-#     qm: float,
-#     cohort_geometry: CohortGeometry,
-#     zm: float,
-# ) -> np.ndarray:
-#     """Solver function for canopy closure height.
-#
-#     This function returns the difference between the total community projected area
-#     at a height $z$ and the total available canopy space for canopy layer $l$, given
-#     the community gap fraction for a given height. It is used with a root solver to
-#     find canopy layer closure heights $z^*_l* for a community.
-#
-#     Args:
-#         m, n, qm : PFT specific shape parameters
-#         H, Ac, zm: stem specific sizes
-#         A, l: cell area and layer index
-#         fG: community gap fraction
-#         :param qm:
-#     """
-#
-#     # Calculate Ap(z)
-#     Ap_z = calculate_projected_canopy_area_profile_for_individual(
-#         z=z, cohort_geometry=cohort_geometry, m=m, n=n, qm=qm, zm=zm
-#     )
-#
-#     # Return the difference between the projected area and the available space
-#     return Ap_z.sum() - (A * l) * (1 - fG)
-#
-#
-# def calculate_canopy_heights(
-#     A: float,
-#     fG: float,
-#     m: Stems,
-#     n: Stems,
-#     qm: Stems,
-#     pft,
-#     zm: Stems,
-# ):
-#
-#     # Calculate the number of layers
-#     total_community_ca = pft.crown_area.sum()
-#     n_layers = int(np.ceil(total_community_ca / (A * (1 - fG))))
-#
-#     # Data store for z*
-#     z_star = np.zeros(n_layers)
-#
-#     # Loop over the layers TODO - edge case of completely filled final layer
-#     for lyr in np.arange(n_layers - 1):
-#         z_star[lyr] = root_scalar(
-#             solve_canopy_closure_height,
-#             args=(lyr + 1, A, fG, m, n, qm, pft, zm),
-#             bracket=(0, pft.height.max()),
-#         ).root
-#
-#     return z_star
+    Args:
+        :param z: array of heights
+        :param cohort_geometry: calculated geometry of cohort
+        :param pft: plant functional type
+    """
+    q_m = calculate_q_m(cohort_geometry.pft.m, cohort_geometry.pft.n)
+    z_m, r_0 = calculate_stem_canopy_factors(cohort_geometry)
+
+    # Calculate q(z)
+    q_z = calculate_relative_canopy_radius(
+        z, cohort_geometry.height, cohort_geometry.pft.m, cohort_geometry.pft.n
+    )
+
+    # Calculate Ap given z > z_m
+    A_p = cohort_geometry.crown_area * (q_z / q_m) ** 2
+    # Set Ap = Ac where z <= z_m
+    A_p = np.where(z <= z_m, cohort_geometry.crown_area, A_p)
+    # Set Ap = 0 where z > H
+    A_p = np.where(z > cohort_geometry.height, 0, A_p)
+
+    return A_p
 
 
 class Canopy:
@@ -350,11 +296,89 @@ class Canopy:
     def __init__(self, community: Community) -> None:
         """placeholder."""
         self.community = community
-        # self.canopy_layer_heights = self.calculate_canopy_layer_heights(
-        #     community.community_geometry
-        # )
+        self.cohort_geometries = community.cohort_geometries
+        self.max_individual_height = max(
+            cohort.height for cohort in self.cohort_geometries
+        )
 
-    def calculate_canopy_layer_heights(
-        self, community_geometry: CommunityGeometry
-    ) -> None:
-        """placeholder."""
+        self.canopy_layer_heights = self.calculate_canopy_layer_heights()
+
+    def solve_canopy_closure_height(
+        self,
+        z: float,
+        l: int,
+        A: float,
+        fG: float,
+    ) -> np.ndarray:
+        """Solver function for canopy closure height.
+
+        This function returns the difference between the total community projected area
+        at a height $z$ and the total available canopy space for canopy layer $l$, given
+        the community gap fraction for a given height. It is used with a root solver to
+        find canopy layer closure heights $z^*_l* for a community.
+
+        Args:
+            A: community area
+            l: layer index
+            fG: community gap fraction
+            :param z:
+            :param qm:
+        """
+
+        cohort_areas_at_z = map(
+            lambda cohort: cohort.number_of_members
+            * calculate_projected_canopy_area_for_individual(z, cohort),
+            self.cohort_geometries,
+        )
+
+        total_projected_area_at_z = sum(cohort_areas_at_z)
+
+        # Return the difference between the projected area and the available space
+        return total_projected_area_at_z - (A * l) * (1 - fG)
+
+    def calculate_canopy_layer_heights(self, A: float, fG: float):
+        # Calculate the number of layers
+        cohort_crown_areas = map(
+            lambda cohort: cohort.number_of_members * cohort.crown_area,
+            self.cohort_geometries,
+        )
+        total_community_crown_area = sum(cohort_crown_areas)
+        number_of_layers = int(np.ceil(total_community_crown_area / (A * (1 - fG))))
+
+        # Data store for z*
+        z_star = np.zeros(number_of_layers)
+
+        # Loop over the layers TODO - edge case of completely filled final layer
+        for n in np.arange(number_of_layers - 1):
+            z_star[n] = root_scalar(
+                self.solve_canopy_closure_height,
+                args=(n + 1, A, fG),
+                bracket=(0, self.max_individual_height),
+            ).root
+
+        return z_star
+
+    # def calculate_canopy_layer_heights(
+    #         self, community_geometry: CohortGeometries
+    # ) -> None:
+    #     """placeholder."""
+
+
+if __name__ == "__main__":
+
+    with open("pyrealm_build_data/community/pfts.json") as f:
+        pfts_json = json.load(f)
+    pfts = PlantFunctionalType.schema().load(pfts_json, many=True)
+    print(pfts)
+
+    flora = Flora(pfts)
+
+    with open("pyrealm_build_data/community/communities.json") as f:
+        communities_json = json.load(f)
+    imported_communities = ImportedCommunity.schema().load(communities_json, many=True)
+    print(imported_communities)
+
+    for imported_community in imported_communities:
+        community = Community(flora, imported_community.cohorts)
+        canopy = Canopy(community)
+        print(canopy.canopy_layer_heights)
