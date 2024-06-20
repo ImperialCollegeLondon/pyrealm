f4506cfe0a90d341950f413d785c7460877ea543
Surbhi Goel <surbhigoel77@github.com>
2024-06-20 09:13:20 +0100

    Removed noqa directive

--- tests/unit/pmodel/test_calc_carbon_isotopes.py
+++ tests/unit/pmodel/test_calc_carbon_isotopes.py
@@ -8,8 +8,6 @@ and shouldn't change trivially.
 Runs a simple scalar test for each of the different optchi methods.
 """
 
-# flake8: noqa: D103  # directive properly formatted
-
 import numpy as np
 import pytest
 
