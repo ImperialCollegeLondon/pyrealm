d4d7362907cee4aacbda56228ceb1449feede2c5
Amy Pike <amymaypike@hotmail.co.uk>
2024-07-19 13:00:02 +0100

    test for happy path for calculating q_m

--- tests/unit/canopy_model/test_jaideeps_t_model_extension.py
+++ tests/unit/canopy_model/test_jaideeps_t_model_extension.py
@@ -6,9 +6,9 @@ from pyrealm.canopy_model.model.jaideep_t_model_extension import *
 
 
 def test_calculate_q_m_values_returns_q_m_for_valid_input():
-    m_values = np.array()
-    n_values = np.array()
-    expected_q_m_values = np.array()
+    m_values = np.array([2, 3])
+    n_values = np.array([5, 4])
+    expected_q_m_values = np.array([2.9038988210485766, 2.3953681843215673])
     actual_q_m_values = calculate_q_m_values(m_values, n_values)
     assert_array_equal(actual_q_m_values, expected_q_m_values)
 
