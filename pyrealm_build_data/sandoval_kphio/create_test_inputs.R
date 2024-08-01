fce92d9e73d8c02081af21b2466eba26b81c0ea0
David Orme <davidorme@users.noreply.github.com>
2024-08-01 12:32:04 +0100

    Better R in sandoval_kphio

--- pyrealm_build_data/sandoval_kphio/create_test_inputs.R
+++ pyrealm_build_data/sandoval_kphio/create_test_inputs.R
@@ -1,24 +1,23 @@
 source("calc_phi0.R")
 
+# Generate  all combinations of some simple input values
 aridity_index <- c(0.2, 0.5, 0.8, 1.0, 5.0, 10.0)
 temp <- seq(0, 50, by = 1)
 mean_gdd_temp <- seq(5, 30, by = 5)
 
 data <- expand.grid(
-    temp = temp,
-    aridity_index = aridity_index,
-    mean_gdd_temp = mean_gdd_temp
+  temp = temp,
+  aridity_index = aridity_index,
+  mean_gdd_temp = mean_gdd_temp
 )
 
-# Function is not parallelised so loop over inputs
-data$phio <- NA
+# Run the reference implementation, which is not parallelised so need mapply
+data$phio <- round(
+  mapply(
+    calc_phi0, data$aridity_index, data$temp, data$mean_gdd_temp
+  ),
+  digits = 8
+)
 
-for (row_idx in seq_along(data$aridity_index)) {
-    data$phio[row_idx] <- with(
-        data[row_idx, ],
-        calc_phi0(AI = aridity_index, tc = temp, mGDD0 = mean_gdd_temp)
-    )
-}
-
-data$phio <- round(data$phio, digits = 8)
+# Save reference dataset.
 write.csv(data, "sandoval_kphio.csv", row.names = FALSE)
