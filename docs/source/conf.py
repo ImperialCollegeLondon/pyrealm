d00331e40d677e8f8a1aa9f8b3ecd48b0caa1ae3
David Orme <davidorme@users.noreply.github.com>
2024-08-08 12:19:04 +0100

    Do _not_ jupytext the Sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: all
-#     notebook_metadata_filter: -jupytext.text_representation.jupytext_version,settings,mystnb
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-# ---
-
 """Configure the Sphinx documentation builder.
 
 -- Path setup --------------------------------------------------------------
