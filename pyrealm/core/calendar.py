1d59218deb976df4a7d75e15ff3c086f763bd4bf
James Emberton <jamie.emberton@gmail.com>
2024-09-16 15:14:23 +0100

    trying a new fix

--- pyrealm/core/calendar.py
+++ pyrealm/core/calendar.py
@@ -184,6 +184,8 @@ class LocationDateTime:
     def decimal_hour(self) -> np.ndarray:
         """Convert year_date_time to a decimal representation of hours.
 
+        .. :no-index:
+
         This method extracts the hours and minutes from the `year_date_time` attribute
         and converts them into a decimal representation of hours.
 
