8b359257e64523daa37fb55068f0934a38bbbb14
James Emberton <jamie.emberton@gmail.com>
2024-10-07 17:23:01 +0100

    moved type hints from signature to params

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,31 +13,24 @@ from dataclasses import dataclass, field
 from datetime import datetime
 
 import sphinxcontrib.bibtex.plugin
+from sphinx.application import Sphinx
 from sphinxcontrib.bibtex.style.referencing import BracketStyle
 from sphinxcontrib.bibtex.style.referencing.author_year import AuthorYearReferenceStyle
 
 from pyrealm import __version__ as pyrealm_version
 
+# Add the project's root directory to the sys.path
 sys.path.insert(0, os.path.abspath("../"))
 
-
 # -- Project information -----------------------------------------------------
-
 project = "pyrealm: Ecosystem Models in Python"
-
 current_year = datetime.today().strftime("%Y")
 copyright = "2020-" + current_year + ", Pyrealm Developers"
 author = "Pyrealm Developers"
-
-# The full version, including alpha/beta/rc tags
 version = pyrealm_version
 release = pyrealm_version
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "autodocsumm",
@@ -49,21 +42,18 @@ extensions = [
     "myst_nb",
     "sphinx_rtd_theme",
     "sphinx_external_toc",
+    # "sphinx_autodoc_typehints",  # For type hints formatting
 ]
 
 # External table of contents
-external_toc_path = "_toc.yml"  # optional, default: _toc.yml
-external_toc_exclude_missing = False  # optional, default: False
+external_toc_path = "_toc.yml"
+external_toc_exclude_missing = False
+
 
 # Citation styling
-
-
 def bracket_style() -> BracketStyle:
     """Custom citation parenthesis style."""
-    return BracketStyle(
-        left="(",
-        right=")",
-    )
+    return BracketStyle(left="(", right=")")
 
 
 @dataclass
@@ -81,17 +71,13 @@ sphinxcontrib.bibtex.plugin.register_plugin(
     "sphinxcontrib.bibtex.style.referencing", "author_year_round", MyReferenceStyle
 )
 
-bibtex_reference_style = "author_year_round"
-
-
 # Cross-reference checking
-# TODO - find some better solution than this to all of these bizarre cross reference
-#        problems.
 nitpicky = True
 nitpick_ignore = [
     ("py:class", "numpy._typing._array_like._ScalarType_co"),
     ("py:class", "numpy._typing._generic_alias.ScalarType"),
     ("py:class", "numpy.float32"),
+    ("py:class", "numpy.int64"),
     ("py:class", "numpy.timedelta64"),
     ("py:class", "numpy.bool_"),
     ("py:class", "numpy.ndarray"),
@@ -99,47 +85,6 @@ nitpick_ignore = [
     ("py:class", "numpy.dtype[+ScalarType]"),
     ("py:class", "numpy.typing.NDArray"),
     ("py:class", "dataclasses.InitVar"),
-    (
-        "py:class",
-        "dataclasses.InitVar[numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]]]",
-    ),
-    (
-        "py:class",
-        "dataclasses.InitVar[numpy.ndarray[typing.Any, numpy.dtype[+_ScalarType_co]]]",
-    ),
-    (
-        "py:class",
-        (
-            "tuple[numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]]]"
-        ),
-    ),
-    (
-        "py:obj",
-        (
-            "typing.Union[~numpy.ndarray[~typing.Any, "
-            "~numpy.dtype[~numpy._typing._generic_alias.ScalarType]], "
-            "tuple[numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]]]]"
-        ),
-    ),
-    (
-        "py:class",
-        (
-            "tuple[numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]]]"
-        ),
-    ),
-    (
-        "py:class",
-        (
-            "tuple[numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]], "
-            "numpy.ndarray[typing.Any, numpy.dtype[+ScalarType]]]"
-        ),
-    ),
 ]
 
 intersphinx_mapping = {
@@ -147,9 +92,9 @@ intersphinx_mapping = {
     "numpy": ("https://numpy.org/doc/stable/", None),
     "python": ("https://docs.python.org/3/", None),
     "xarray": ("https://docs.xarray.dev/en/stable/", None),
+    "pandas": ("http://pandas.pydata.org/pandas-docs/dev/", None),
     "shapely": ("https://shapely.readthedocs.io/en/stable/", None),
 }
-
 
 autodoc_default_flags = ["members"]
 autosummary_generate = True
@@ -161,56 +106,38 @@ myst_heading_anchors = 4
 mathjax3_config = {
     "tex": {
         "extensions": ["mhchem.js"],
-        # 'inlineMath': [['$', '$']]
     }
 }
 
-# Turn off ugly rendering of class attributes
+# Napoleon settings
 napoleon_use_ivar = True
 napoleon_custom_sections = [("PModel Parameters", "params_style")]
 
 # Autodoc configuration:
-# - Suppress signature expansion of arguments
 autodoc_preserve_defaults = True
-# - Have funcname not pyrealm.pmodel.funcname in autodoc
 add_module_names = False
-# - Group members by type not alphabetically
+# napoleon_use_ivar = True
+napoleon_preprocess_types = True
+napoleon_use_param = True
+napoleon_use_rtype = False
+
 autodoc_member_order = "groupwise"
-
-# Control how type hints are shown in the generated documentation
-autodoc_typehints_format = "short"  # Use short format for type hints
-
-# Use unqualified names for types (i.e., without the module prefix)
-python_use_unqualified_type_names = True
-
-napoleon_use_param = False  # Do not show type hints in function signatures
-napoleon_use_rtype = False  # Do not show return types in function signatures
+autodoc_typehints_format = "short"
+autodoc_typehints = "description"
 
 bibtex_bibfiles = ["refs.bib"]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["maxime*", "**.ipynb_checkpoints"]
+html_static_path = ["_static"]
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-
-# html_theme = 'sphinx_material'
 html_theme = "sphinx_rtd_theme"
-
 html_theme_options = {
     "logo_only": False,
     "display_version": True,
     "prev_next_buttons_location": "top",
     "style_external_links": False,
     "style_nav_header_background": "grey",
-    # Toc options
     "collapse_navigation": False,
     "sticky_navigation": True,
     "navigation_depth": 4,
@@ -219,36 +146,8 @@ html_theme_options = {
 }
 
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+def setup(app: Sphinx):  # type: ignore
+    """Setup function to connect the event handler to autodoc-process-docstring."""
 
-
-# Custom logic to simplify specific types in type annotations
-def simplify_type_hints(signature: str, return_annotation: str) -> tuple[str, str]:
-    """Simplifies function signatures for documentation."""
-    # Replace verbose numpy type hints with simpler versions
-    if signature:
-        signature = signature.replace("~numpy.ndarray", "ndarray")
-        signature = signature.replace("~typing.Any", "Any")
-        signature = signature.replace("~numpy.dtype[~numpy.float32]", "float32")
-    if return_annotation:
-        return_annotation = return_annotation.replace("~numpy.ndarray", "ndarray")
-        return_annotation = return_annotation.replace("~typing.Any", "Any")
-        return_annotation = return_annotation.replace(
-            "~numpy.dtype[~numpy.float32]", "float32"
-        )
-    return signature, return_annotation
-
-
-def setup(app):  # type: ignore
-    """Use setup to remove .ipynb from sources.
-
-    Note that this assumes that all ipynb files are paired with Myst Markdown
-    files via Jupytext
-    """
     # Ignore .ipynb files
     app.registry.source_suffix.pop(".ipynb", None)
-    # Connect the event to modify the type hints
-    app.connect("autodoc-process-signature", simplify_type_hints)
