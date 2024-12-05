606336306fa06560b490804c9ecdd7fb0d593ce2
David Orme <davidorme@users.noreply.github.com>
2024-12-05 16:18:30 +0000

    Adding more growing season functionality

--- pyrealm/phenology/growing_season.py
+++ pyrealm/phenology/growing_season.py
@@ -14,6 +14,8 @@ for a location.
 import numpy as np
 from numpy.typing import NDArray
 
+from pyrealm.core.calendar import Calendar
+
 
 def filter_short_intervals_strides(
     ts: NDArray[np.bool_], window: int
@@ -39,6 +41,8 @@ def filter_short_intervals_strides(
         window: The maximum interval length to remove
         axis: The axis along which to filter intervals
     """
+
+    raise NotImplementedError("The implementation on this function is incomplete.")
 
     # Draws from: https://stackoverflow.com/a/52219082/3401916
 
@@ -125,3 +129,124 @@ def run_length_encode(values: NDArray) -> tuple[NDArray[np.int_], NDArray]:
     run_lengths = np.diff(np.append(-1, change_points))
 
     return (run_lengths, values[change_points])
+
+
+def find_cumulative_suitable_days(
+    suitable: NDArray[np.bool_],
+) -> NDArray[np.int_]:
+    """Calculate runs of consecutive suitable days.
+
+    This functions takes an boolean array representing a daily time series of whether
+    growing conditions are suitable for each day. It calculates the cumulative sum of
+    the number of consecutive days of suitable growth, resetting the sum to zero when an
+    unsuitable day is encountered.
+
+    Examples:
+        >>> suitable = np.array([0, 0, 1, 1, 1, 0, 1, 1, 0])
+        >>> accumulate_consecutive_suitable_days(suitable)
+        [0, 0, 1, 2, 3, 0, 1, 2, 0]
+
+    Args:
+        suitable: A boolean array of growth suitability data.
+    """
+    # Calculate a cumulative sum of suitable days
+    cumulative_suitable_days = np.cumsum(suitable, axis=0, dtype="int")
+
+    # Find the cumulative sum of consecutive suitable days. The accumulation resets
+    # the count to zero when an unsuitable day is encountered.
+    consecutive_suitable_days = cumulative_suitable_days - np.maximum.accumulate(
+        np.where(np.logical_not(suitable), cumulative_suitable_days, 0), axis=0
+    )
+
+    return consecutive_suitable_days
+
+
+def find_annual_growing_season(
+    suitable: NDArray[np.bool_],
+    dates: NDArray[np.datetime64],
+    return_dates: bool = False,
+) -> (
+    NDArray[np.int_]
+    | tuple[NDArray[np.datetime64], NDArray[np.datetime64], NDArray[np.int_]]
+):
+    """Find the growing season from annual suitability data.
+
+    This function takes an boolean array representing a daily time series for a single
+    year of whether growing conditions are suitable for each day. It calculates the
+    length of the maximum run of suitable days and, optionally, also returns the start
+    and end dates of the growing season.
+
+    Args:
+        suitable: A boolean array of growth suitability data.
+        dates: A numpy array of dates as ``np.datetime64``.
+        return_dates: Should the function return the start and end dates of the season
+            as well as the season length.
+    """
+
+    # Find the cumulative consecutive suitable days for the year
+    year_consecutive = find_cumulative_suitable_days(suitable)
+
+    # Find the index of the maximum and use it to extract the season length and the
+    # season start and end dates
+    year_max_consecutive_idx = np.argmax(year_consecutive, axis=0, keepdims=True)
+    season_length = np.take_along_axis(
+        year_consecutive, year_max_consecutive_idx, axis=0
+    ).squeeze()
+
+    # Just return the season lengths
+    if not return_dates:
+        return season_length
+
+    # Calculate the season start and end dates if requested
+    season_end = dates[year_max_consecutive_idx].squeeze()
+    season_start = dates[year_max_consecutive_idx - season_length + 1].squeeze()
+
+    return season_start, season_end, season_length
+
+
+def find_growing_seasons(
+    suitable: NDArray[np.bool_],
+    dates: NDArray[np.datetime64],
+    return_dates: bool = False,
+) -> (
+    NDArray[np.int_]
+    | tuple[NDArray[np.datetime64], NDArray[np.datetime64], NDArray[np.int_]]
+):
+    """Find growing seasons across multiple years.
+
+    This function takes an boolean array representing a multiple year time series of
+    daily data showing whether growing conditions are suitable for each day. It
+    calculates the length of the maximum run of suitable days within each year and,
+    optionally, also returns the start and end dates of the growing season.
+
+    Args:
+        suitable: A boolean array of growth suitability data.
+        dates: A numpy array of dates as ``np.datetime64``.
+        return_dates: Should the function return the start and end dates of the season
+            as well as the season length.
+    """
+    # TODO need to add hemisphere switching
+
+    # Get the dates as a Calendar object
+    calendar = Calendar(dates)
+
+    if dates.shape != suitable[0].shape:
+        raise ValueError("Number of dates not equal to first axis length of suitable")
+
+    # Split the input arrays along the first axis to give blocks of suitability and
+    # dates for each year
+    year_change_indices = np.where(np.diff(calendar.year) > 0)[0]
+    year_subarrays = np.split(suitable, year_change_indices)
+    date_subarrays = np.split(dates, year_change_indices)
+
+    # Calculate the annual growing season within each year split
+    seasons = [
+        find_annual_growing_season(
+            suitable=year_array, dates=date_array, return_dates=return_dates
+        )
+        for year_array, date_array in zip(year_subarrays, date_subarrays)
+    ]
+
+    # Repackage into arrays
+
+    return seasons
--- tests/unit/phenology/test_growing_season.py
+++ tests/unit/phenology/test_growing_season.py
@@ -4,6 +4,7 @@ from contextlib import nullcontext as does_not_raise
 
 import numpy as np
 import pytest
+from numpy.testing import assert_allclose, assert_equal
 
 
 @pytest.mark.parametrize(
@@ -43,7 +44,7 @@ def test_filter_short_intervals(ts_reshape, ts_broadcast):
         for win in windows[win_length:]:
             expected[[win], ...] = 0
 
-        assert np.allclose(result, expected)
+        assert_allclose(result, expected)
 
 
 @pytest.mark.parametrize(
@@ -83,7 +84,132 @@ def test_run_length_encode(input, outcome, err_msg):
         run_lengths, run_values = run_length_encode(input)
 
         # Check that the encoding can be used to reconstruct the original.
-        assert np.allclose(input, np.repeat(run_values, run_lengths))
+        assert_allclose(input, np.repeat(run_values, run_lengths))
         return
 
     assert str(err.value) == err_msg
+
+
+@pytest.mark.parametrize(
+    argnames="ts_reshape, ts_broadcast",
+    argvalues=[
+        pytest.param((21,), (21,), id="row_vector"),
+        pytest.param((21, 1), (21, 1), id="column_vector"),
+        pytest.param((21, 1), (21, 2), id="2D"),
+        pytest.param((21, 1, 1), (21, 2, 2), id="3D"),
+    ],
+)
+def test_find_cumulative_suitable_days(ts_reshape, ts_broadcast):
+    """Test find_cumulative_suitable_days.
+
+    Uses a simple test case to check calculation works across inputs of differing
+    dimensions
+    """
+    from pyrealm.phenology.growing_season import find_cumulative_suitable_days
+
+    inputs = np.array([0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0])
+    expected = np.array([0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 1, 2, 3, 4, 0, 0, 1, 2, 0, 1, 0])
+
+    # Broadcast the data into different dimensionalities
+    inputs_reshape = np.broadcast_to(inputs.reshape(ts_reshape), ts_broadcast)
+    expected_reshape = np.broadcast_to(expected.reshape(ts_reshape), ts_broadcast)
+
+    # Check the calculation works
+    calculated = find_cumulative_suitable_days(inputs_reshape)
+
+    assert_allclose(calculated, expected_reshape)
+
+
+@pytest.mark.parametrize(
+    argnames="ts_reshape, ts_broadcast",
+    argvalues=[
+        pytest.param((21,), (21,), id="row_vector"),
+        pytest.param((21, 1), (21, 1), id="column_vector"),
+        pytest.param((21, 1), (21, 2), id="2D"),
+        pytest.param((21, 1, 1), (21, 2, 2), id="3D"),
+    ],
+)
+def test_find_annual_growing_season(ts_reshape, ts_broadcast):
+    """Test find_annual_growing_season.
+
+    Uses a simple test case to check calculation works across inputs of differing
+    dimensions.
+    """
+    from pyrealm.phenology.growing_season import find_annual_growing_season
+
+    inputs = np.array([0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0])
+    dates = np.arange(
+        np.datetime64("2024-06-01"), np.datetime64("2024-06-21"), np.timedelta64(1, "D")
+    )
+
+    # Broadcast the data into different dimensionalities
+    inputs_reshape = np.broadcast_to(inputs.reshape(ts_reshape), ts_broadcast)
+    expected_season_length = np.full_like(inputs_reshape[0], 4)
+    expected_season_start = np.full_like(
+        inputs_reshape[0], np.datetime64("2024-06-04"), dtype="datetime64[D]"
+    )
+    expected_season_end = np.full_like(
+        inputs_reshape[0], np.datetime64("2024-06-07"), dtype="datetime64[D]"
+    )
+
+    # Check the calculation works
+    season_start, season_end, season_length = find_annual_growing_season(
+        inputs_reshape, dates, return_dates=True
+    )
+
+    # All integers and dates so use assert_equal
+    assert_equal(season_length, expected_season_length)
+    assert_equal(season_start, expected_season_start)
+    assert_equal(season_end, expected_season_end)
+
+
+def test_find_annual_growing_season_complex():
+    """Test find_annual_growing_season.
+
+    Uses a more 3D complex input where the results should differ across the cells.
+    """
+    from pyrealm.phenology.growing_season import find_annual_growing_season
+
+    # Construct 4 time series
+
+    dates = np.arange(
+        np.datetime64("2021-01-01"), np.datetime64("2021-12-31"), np.timedelta64(1, "D")
+    )
+
+    growing_seasons = (
+        (np.datetime64("2021-06-01"), np.datetime64("2021-07-31")),
+        (np.datetime64("2021-05-01"), np.datetime64("2021-08-31")),
+        (np.datetime64("2021-04-01"), np.datetime64("2021-09-30")),
+        (np.datetime64("2021-03-01"), np.datetime64("2021-10-31")),
+    )
+
+    # Fill in ones between those dates on each time series in a 2x2 grid
+    inputs = np.zeros((365, 2, 2))
+    for (grow_start, grow_end), idx in zip(
+        growing_seasons, np.ndindex(inputs[0].shape)
+    ):
+        inputs[
+            slice(
+                np.where(dates == grow_start)[0][0], np.where(dates == grow_end)[0][0]
+            ),
+            *idx,
+        ] = 1
+
+    # Get the expectations
+    expected_season_length = np.array(
+        [(b - a) / np.timedelta64(1, "D") for a, b in growing_seasons], dtype="int"
+    ).reshape((2, 2))
+
+    growing_seasons_array = np.array(growing_seasons).reshape((2, 2, 2))
+    expected_season_start = growing_seasons_array[:, :, 0]
+    expected_season_end = growing_seasons_array[:, :, 1] - np.timedelta64(1, "D")
+
+    # Check the calculation works
+    season_start, season_end, season_length = find_annual_growing_season(
+        inputs, dates, return_dates=True
+    )
+
+    # All integers and dates so use assert_equal
+    assert_equal(season_length, expected_season_length)
+    assert_equal(season_start, expected_season_start)
+    assert_equal(season_end, expected_season_end)
