0b87b85a1cbc8315cf72f86d12f96e57f5d0d729
David Orme <davidorme@users.noreply.github.com>
2024-12-11 18:39:28 +0000

    Adding AI and soil moisture

--- tests/regression/phenology/conftest.py
+++ tests/regression/phenology/conftest.py
@@ -1,5 +1,6 @@
 """Shared fixtures for phenology testing."""
 
+import json
 from importlib import resources
 
 import pandas as pd
@@ -16,12 +17,18 @@ def de_gri_half_hourly_data():
         / "FLX_DE-Gri_FLUXNET2015_phenology_inputs.csv"
     )
 
-    de_gri_data = pd.read_csv(datapath, na_values=[-9999])
+    de_gri_data = pd.read_csv(datapath, na_values=[-9999, -9999.0])
 
     # Calculate time as np.datetime64 and scale other vars
     de_gri_data["time"] = pd.to_datetime(
         de_gri_data["TIMESTAMP_START"], format="%Y%m%d%H%M"
     )
+
+    # # Blank out temperatures under 25°C
+    # de_gri_data["TA_F"] = de_gri_data["VPD_F"].where(de_gri_data["VPD_F"] >= -25)
+
+    # # # VPD from hPa to Pa
+    # de_gri_data["VPD_F"] = de_gri_data["VPD_F"].to_numpy() * 100
     # Pressure from kPa to Pa
     de_gri_data["PA_F"] = de_gri_data["PA_F"].to_numpy() * 1000
     # PPFD from SWDOWN
@@ -48,3 +55,16 @@ def de_gri_daily_data():
     de_gri_data["PA_F"] = de_gri_data["PA_F"].to_numpy() * 1000
 
     return de_gri_data
+
+
+@pytest.fixture
+def de_gri_constants():
+    """Load the site constants."""
+
+    # Load the data
+    datapath = resources.files("pyrealm_build_data.phenology") / "DE-GRI_site_data.json"
+
+    with open(datapath, "rb") as site_file:
+        constants = json.load(site_file)
+
+    return constants
--- tests/regression/phenology/test_phenology_gpp.py
+++ tests/regression/phenology/test_phenology_gpp.py
@@ -2,14 +2,20 @@
 
 import numpy as np
 import pandas as pd
-import pytest
+from numpy.testing import assert_allclose
 
 
-@pytest.mark.skip("Need to resolve calculation process - currently not matching")
-def test_phenology_gpp_calculation(de_gri_half_hourly_data):
+# @pytest.mark.skip(
+#     "Need to resolve calculation process - currently not matching due to aridity "
+#     "penalty applied to GPP in input data."
+# )
+def test_phenology_gpp_calculation(
+    de_gri_daily_data, de_gri_half_hourly_data, de_gri_constants
+):
     """Test the provided GPP values for phenology can be recreated."""
 
     from pyrealm.pmodel import PModel, PModelEnvironment, SubdailyPModel, SubdailyScaler
+    from pyrealm.pmodel.functions import calc_soilmstress_mengoli
 
     # Calculate the PModel photosynthetic environment
     env = PModelEnvironment(
@@ -19,11 +25,24 @@ def test_phenology_gpp_calculation(de_gri_half_hourly_data):
         patm=de_gri_half_hourly_data["PA_F"].to_numpy(),
     )
 
+    de_gri_pmodel = PModel(
+        env=env,
+        # reference_kphio=1 / 8,
+    )
+
+    de_gri_pmodel.estimate_productivity(
+        fapar=np.ones_like(env.ca),
+        ppfd=de_gri_half_hourly_data["SW_IN_F"].to_numpy() * 2.04,
+    )
+
+    # Currently close but not exact
+    assert_allclose(de_gri_pmodel.gpp, de_gri_half_hourly_data["A0_normal"].to_numpy())
+
     # Set up the datetimes of the observations and set the acclimation window
     scaler = SubdailyScaler(datetimes=de_gri_half_hourly_data["time"].to_numpy())
     scaler.set_window(
         window_center=np.timedelta64(12, "h"),
-        half_width=np.timedelta64(31, "m"),
+        half_width=np.timedelta64(30, "m"),
     )
 
     # Fit the potential GPP: fAPAR = 1 and phi0 = 1/8
@@ -35,22 +54,15 @@ def test_phenology_gpp_calculation(de_gri_half_hourly_data):
         reference_kphio=1 / 8,
     )
 
-    de_gri_pmodel = PModel(
-        env=env,
-        reference_kphio=1 / 8,
+    # Calculate soil moisture stress factor
+    soilm_stress = calc_soilmstress_mengoli(
+        soilm=de_gri_daily_data["soilm"].to_numpy(),
+        aridity_index=de_gri_constants["AI_from_coords"],
     )
 
-    de_gri_pmodel.estimate_productivity(
-        fapar=np.ones_like(env.ca),
-        ppfd=de_gri_half_hourly_data["SW_IN_F_MDS"].to_numpy() * 2.04,
-    )
-
-    # Currently close but not exact
     assert np.allclose(
-        de_gri_pmodel.gpp, de_gri_half_hourly_data["A0_normal"].to_numpy()
-    )
-    assert np.allclose(
-        de_gri_subdaily_pmodel.gpp, de_gri_half_hourly_data["A0_slow"].to_numpy()
+        de_gri_subdaily_pmodel.gpp * soilm_stress,
+        de_gri_half_hourly_data["A0_slow"].to_numpy(),
     )
 
 
@@ -60,6 +72,8 @@ def test_daily_values(de_gri_half_hourly_data, de_gri_daily_data):
     This is just to validate that daily and annual values in the de_gri_daily_data
     inputs can be correctly derived from the original half hourly inputs.
     """
+
+    from pyrealm.phenology.growing_season import filter_short_intervals
 
     # Set the time as the index to enable temporal resampling.
     de_gri_half_hourly_data = de_gri_half_hourly_data.set_index("time")
@@ -76,25 +90,68 @@ def test_daily_values(de_gri_half_hourly_data, de_gri_daily_data):
     daily_precip = de_gri_daily_resampler["P_F"].sum().to_numpy()
     assert np.allclose(daily_precip, de_gri_daily_data["P_F"], atol=0.01)
 
-    # Annual values for mean CO2 and total precipitation
-    de_gri_annual_resampler = de_gri_half_hourly_data.resample("YE")
+    # Going from daily data to annual average conditions
+    de_gri_daily_data = de_gri_daily_data.set_index("date")
 
-    # Get annual data frame of mean CO2 and total precip
-    annual_CO2 = de_gri_annual_resampler[["CO2_F_MDS"]].mean()
-    annual_precip = de_gri_annual_resampler[["P_F"]].sum()
-    annual = pd.merge(annual_CO2, annual_precip, left_index=True, right_index=True)
-    annual.columns = [v + "_validation" for v in annual.columns]
-    annual = annual.reset_index()
-    annual["year"] = annual["time"].dt.year
+    # Calculate suitable growth days - days within any period of five or more
+    # consecutive days above 0°C are growing days.
+    above_zero_degrees = (de_gri_daily_data["TA_F"] >= 0).to_numpy()
+    not_suitable = filter_short_intervals(
+        np.logical_not(above_zero_degrees),
+        window=4,
+    )
+    de_gri_daily_data["growing_day"] = np.logical_not(not_suitable)
 
-    # Map the annual values onto daily observations by year
-    de_gri_daily_data["year"] = de_gri_daily_data["date"].dt.year
-    de_gri_daily_data = de_gri_daily_data.merge(annual)
+    # Get a sampler on the daily data to calculate annually constant values
+    de_gri_annual_resampler = de_gri_daily_data.resample("YE")
 
-    assert np.allclose(
-        de_gri_daily_data["CO2_F_MDS"], de_gri_daily_data["CO2_F_MDS_validation"]
+    # Subset to the values in the dataset - check there is no variation in the values
+    # before extracting annual values
+    de_gri_annual_values = de_gri_annual_resampler[
+        ["P", "CO2_F_MDS", "VPD", "chi_gst", "Gsl"]
+    ]
+
+    assert np.all(de_gri_annual_values.var() == 0)
+    expected_annual = de_gri_annual_values.mean()
+
+    # Check the calculation from daily data frame:
+    # * mean CO2
+    # * total precipitation
+    # * count of growing days
+
+    # Resampler of filtered values to get VPD and Chi during the growing season
+    de_gri_growing_season_resampler = de_gri_daily_data[
+        de_gri_daily_data["growing_day"]
+    ].resample("YE")
+
+    # Compile annual values
+    calculated_annual = pd.concat(
+        [
+            de_gri_annual_resampler[["P_F"]].sum(),
+            de_gri_annual_resampler[["CO2_F_MDS"]].mean(),
+            de_gri_growing_season_resampler[["VPD_F"]].mean(),
+            de_gri_growing_season_resampler[["VPD_F"]].mean(),  # TODO - change to chi
+            de_gri_annual_resampler[["growing_day"]].sum(),
+        ],
+        axis=1,
     )
 
-    assert np.allclose(
-        de_gri_daily_data["P"], de_gri_daily_data["P_F_validation"], atol=0.01
-    )
+    # Synchronise field names
+    calculated_annual.columns = expected_annual.columns
+
+    # Test calculation with provided absolute tolerances.
+    # TODO - these tolerances are currently very large. Need to resolve issue with
+    # identifying growing season days, which is wrecking the calculation.
+
+    test_tol = {
+        "P": 0.001,
+        "CO2_F_MDS": 0.00001,
+        "VPD": 0.5,  # TODO - fix this
+        "chi_gst": 5,  # TODO - fix this
+        "Gsl": 5,  # TODO - fix this
+    }
+
+    for col in calculated_annual.columns:
+        assert_allclose(
+            expected_annual[col], calculated_annual[col], atol=test_tol[col]
+        )
