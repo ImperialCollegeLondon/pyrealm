26c196585d4a3022830eb3609dc43db3fd41bf08
Marion <56403724+MarionBWeinzierl@users.noreply.github.com>
2025-02-11 14:16:29 +0000

    Accept @davidorme 's suggestion from code review
    Co-authored-by: David Orme <davidorme@users.noreply.github.com>

--- pyrealm/constants/phenology_const.py
+++ pyrealm/constants/phenology_const.py
@@ -13,14 +13,14 @@ class PhenologyConst(ConstantsClass):
     in the phenology module.
     """
 
+    z: float = 12.227
     """z accounts for the costs of building and maintaining leaves and the total
        below-ground allocation required to support the nutrient demand of those
        leaves. [mol m^{-2} year^{-1}]."""
-    z: float = 12.227
 
-    """Light extinction coefficient."""
     k: float = 0.5
+    """Light extinction coefficient."""
 
+    f0_coefficients = (0.65, 0.604169, 1.9)
     """Coefficients to calculate f_0, the ratio of annual total transpiration of annual 
     total precipitation."""
-    f0_coefficients = (0.65, 0.604169, 1.9)
