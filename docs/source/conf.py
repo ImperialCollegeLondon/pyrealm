b7800dda1d052121726cc09db1e692aab5abd6e8
David Orme <davidorme@users.noreply.github.com>
2025-03-14 15:18:58 +0000

    Uncommented one line too many

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -214,7 +214,7 @@ exclude_patterns = ["maxime*", "**.ipynb_checkpoints"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_material"
+# html_theme = "sphinx_material"
 html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
