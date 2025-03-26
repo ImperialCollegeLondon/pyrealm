698d80a3b89fc41fd08f142152cc48b6131418d3
David Orme <davidorme@users.noreply.github.com>
2025-03-26 14:32:28 +0000

    Test updates - skipping from_pmodel for now

--- tests/regression/phenology/test_phenology_faparlimitation.py
+++ tests/regression/phenology/test_phenology_faparlimitation.py
@@ -26,7 +26,8 @@ def annual_data():
     """Load the input data from netcdf file."""
 
     datafile = (
-        resources.files("pyrealm_build_data") / "phenology/python_annual_outputs.csv"
+        resources.files("pyrealm_build_data.phenology.fortnightly_example")
+        / "annual_outputs.csv"
     )
 
     return pd.read_csv(datafile)
@@ -35,20 +36,14 @@ def annual_data():
 def test_faparlimitation(site_data, annual_data):
     """Regression test for FaparLimitation constructor."""
 
-    from pyrealm.constants import CoreConst
     from pyrealm.phenology.fapar_limitation import FaparLimitation
 
-    core_const = CoreConst()
-
     faparlim = FaparLimitation(
-        annual_total_potential_gpp=annual_data[
-            "annual_total_A0_subdaily_with_water_stress"
-        ].to_numpy()
-        / core_const.k_c_molmass,
-        annual_mean_ca=annual_data["annual_mean_ca_in_GS"].to_numpy(),
-        annual_mean_chi=annual_data["annual_mean_chi_in_GS"].to_numpy(),
-        annual_mean_vpd=annual_data["annual_mean_VPD_in_GS"].to_numpy(),
-        annual_total_precip=annual_data["annual_precip_molar"].to_numpy(),
+        annual_total_potential_gpp=annual_data["ann_total_A0"].to_numpy(),
+        annual_mean_ca=annual_data["ca"].to_numpy(),
+        annual_mean_chi=annual_data["chi"].to_numpy(),
+        annual_mean_vpd=annual_data["vpd_mean"].to_numpy(),
+        annual_total_precip=annual_data["precip_molar_sum"].to_numpy(),
         aridity_index=site_data["AI_from_cruts"],
     )
 
@@ -57,63 +52,35 @@ def test_faparlimitation(site_data, annual_data):
 
 
 @pytest.fixture()
-def pmodel_data():
+def fortnightly_data():
     """Load the input data for the from_pmodel class function from netcdf file."""
 
-    datafile_daily = (
-        resources.files("pyrealm_build_data") / "phenology/python_daily_outputs.csv"
+    datafile = (
+        resources.files("pyrealm_build_data.phenology.fortnightly_example")
+        / "fortnightly_data.csv"
     )
 
-    daily_data = pd.read_csv(datafile_daily)
+    data = pd.read_csv(datafile)
 
-    datafile_halfhourly = (
-        resources.files("pyrealm_build_data") / "phenology/python_hh_outputs.csv"
-    )
+    data["time"] = pd.to_datetime(data["time"])
 
-    hh_data = pd.read_csv(datafile_halfhourly)
-
-    return (daily_data, hh_data)
-
-    tc = np.array(hh_data["ta"])
-    vpd = np.array(hh_data["vpd"])
-    co2 = np.array(hh_data["co2"])
-    patm = np.array(hh_data["pa_f"])
-    growing_season = np.array(daily_data["growing_day_filtered"])
-    datetimes = np.array(hh_data["time"], dtype="datetime64")
-    precipitation = np.array(daily_data["pre"])
-    ppfd = np.array(hh_data["ppfd"])
-    fapar_max = np.array(np.unique(daily_data["annual_fapar_max"]))
-    lai_max = np.array(np.unique(daily_data["annual_lai_max"]))
-
-    return (
-        tc,
-        vpd,
-        co2,
-        patm,
-        growing_season,
-        datetimes,
-        precipitation,
-        ppfd,
-        fapar_max,
-        lai_max,
-    )
+    return data
 
 
-def test_faparlimitation_frompmodel(annual_data, site_data, pmodel_data):
+@pytest.mark.skip("Need to expand the time handling to cope with datetimes >= 1 day")
+def test_faparlimitation_frompmodel(annual_data, site_data, fortnightly_data):
     """Regression test for from_pmodel FaparLimitation class method."""
 
     from pyrealm.phenology.fapar_limitation import FaparLimitation
     from pyrealm.pmodel import PModel, PModelEnvironment
 
-    daily_data, hh_data = pmodel_data
-
     env = PModelEnvironment(
-        tc=hh_data["ta"].to_numpy(),
-        vpd=hh_data["vpd"].to_numpy(),
-        co2=hh_data["co2"].to_numpy(),
-        patm=hh_data["pa_f"].to_numpy(),
-        fapar=np.ones_like(hh_data["ta"]),
-        ppfd=hh_data["ppfd"].to_numpy(),
+        tc=fortnightly_data["tc_mean"].to_numpy(),
+        vpd=fortnightly_data["vpd_mean"].to_numpy(),
+        co2=fortnightly_data["co2_mean"].to_numpy(),
+        patm=fortnightly_data["patm_mean"].to_numpy(),
+        fapar=np.ones_like(fortnightly_data["tc_mean"]),
+        ppfd=fortnightly_data["ppfd_mean"].to_numpy(),
     )
 
     pmodel = PModel(
@@ -124,9 +91,9 @@ def test_faparlimitation_frompmodel(annual_data, site_data, pmodel_data):
 
     faparlim = FaparLimitation.from_pmodel(
         pmodel=pmodel,
-        growing_season=daily_data["growing_day_filtered"],
-        datetimes=hh_data["time"],
-        precip=daily_data["pre"],
+        growing_season=fortnightly_data["growing_season"].to_numpy(),
+        datetimes=fortnightly_data["time"].to_numpy(),
+        precip=fortnightly_data["precip_molar_sum"].to_numpy(),
         aridity_index=site_data["AI_from_cruts"],
     )
 
