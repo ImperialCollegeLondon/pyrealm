1a4c09cb3f022cce02cbef046d1e140f6d7e863e
MarionBWeinzierl <mw925@cam.ac.uk>
2025-04-11 17:34:20 +0100

    rewrote the subdaily fapar test

--- tests/regression/phenology/test_phenology_faparlimitation.py
+++ tests/regression/phenology/test_phenology_faparlimitation.py
@@ -83,6 +83,36 @@ def fortnightly_data():
     return data
 
 
+@pytest.fixture()
+def subdaily_data():
+    """Load the input data from data file."""
+
+    datafile = (
+        resources.files("pyrealm_build_data.phenology.subdaily_example")
+        / "half_hourly_data.csv"
+    )
+
+    # Load the half hourly data
+    subdaily_data = pd.read_csv(datafile)
+
+    return subdaily_data
+
+
+@pytest.fixture()
+def daily_data():
+    """Load the input data from data file."""
+
+    datafile = (
+        resources.files("pyrealm_build_data.phenology.subdaily_example")
+        / "daily_outputs.csv"
+    )
+
+    # Load the half hourly data
+    daily_data = pd.read_csv(datafile)
+
+    return daily_data
+
+
 @pytest.mark.skip("Need to expand the time handling to cope with datetimes >= 1 day")
 def test_faparlimitation_frompmodel(annual_data, site_data, fortnightly_data):
     """Regression test for from_pmodel FaparLimitation class method."""
@@ -117,81 +147,26 @@ def test_faparlimitation_frompmodel(annual_data, site_data, fortnightly_data):
     assert np.allclose(annual_data["lai_max"].to_numpy(), faparlim.lai_max)
 
 
-@pytest.fixture()
-def subdaily_data():
-    """Load the input data from data file."""
-
-    from pyrealm.core.water import convert_water_mm_to_moles
-
-    datafile = (
-        resources.files("pyrealm_build_data.phenology") / "DE_GRI_hh_fluxnet_simple.csv"
-    )
-
-    # Load the half hourly data - ignoring mypy's dislike of perfectly functional
-    # numeric inputs to na_values.
-    de_gri_hh_pd = pd.read_csv(
-        datafile,
-        na_values=["-9999-9999.0", -9999.0, -9999],  # type: ignore[list-item]
-    )
-
-    # Calculate time as np.datetime64, set as the index and convert to xarray
-    de_gri_hh_pd["time"] = pd.to_datetime(
-        de_gri_hh_pd["TIMESTAMP_START"], format="%Y%m%d%H%M"
-    )
-    de_gri_hh_pd = de_gri_hh_pd.set_index("time")
-    de_gri_hh_xr = de_gri_hh_pd.to_xarray()
-
-    # # Blank out temperatures under 25°C
-    de_gri_hh_xr["TA_F"] = de_gri_hh_xr["TA_F"].where(de_gri_hh_xr["TA_F"] >= -25)
-
-    # # VPD from hPa to Pa
-    de_gri_hh_xr["VPD_F"] = de_gri_hh_xr["VPD_F"] * 100
-    # Pressure from kPa to Pa
-    de_gri_hh_xr["PA_F"] = de_gri_hh_xr["PA_F"] * 1000
-    # PPFD from SWDOWN
-    de_gri_hh_xr["PPFD"] = de_gri_hh_xr["SW_IN_F_MDS"] * 2.04
-
-    # Convert precipitation to molar values at half hour scale to aggregate up to annual
-    # totals. Can't simply convert annual means here - need to convert with conditions
-    # at half hourly time step.
-    #
-    # - Both FluxNET and CRU (loaded below for aridity and soil moisture calculations)
-    #   provide precipitation data. CRU is more consistent with the aridity index
-    #   calculation and hence f_0, but the FluxNET data is more site appropriate so is
-    #   used here. We also need Temp and PATM to convert water mm to water mols, and
-    #   currently the soil moisture inputs don't include that from the daily CRU data.
-
-    # Calculate water as mols m2 not mm m2
-    site_precip_molar = convert_water_mm_to_moles(
-        water_mm=de_gri_hh_xr["P_F"].to_numpy(),
-        tc=de_gri_hh_xr["TA_F"].to_numpy(),
-        patm=de_gri_hh_xr["PA_F"].to_numpy(),
-    )
-
-    de_gri_hh_xr = de_gri_hh_xr.assign(P_F_MOLAR=("time", site_precip_molar))
-
-    return de_gri_hh_xr
-
-
-def test_faparlimitation_fromsubdailypmodel(site_data, splash_data, subdaily_data):
+def test_faparlimitation_fromsubdailypmodel(site_data, subdaily_data, daily_data):
     """Regression test for from_subdailypmodel FaparLimitation class method."""
 
     from pyrealm.phenology.fapar_limitation import FaparLimitation, daily_to_subdaily
     from pyrealm.pmodel import AcclimationModel, PModelEnvironment, SubdailyPModel
-    from pyrealm.pmodel.functions import calc_soilmstress_mengoli
 
     env = PModelEnvironment(
-        tc=subdaily_data["TA_F"].to_numpy(),
-        vpd=subdaily_data["VPD_F"].to_numpy(),
-        co2=subdaily_data["CO2_F_MDS"].to_numpy(),
-        patm=subdaily_data["PA_F"].to_numpy(),
-        fapar=np.ones_like(subdaily_data["TA_F"]),
-        ppfd=subdaily_data["PPFD"].to_numpy(),
+        tc=subdaily_data["tc"].to_numpy(),
+        vpd=subdaily_data["vpd"].to_numpy(),
+        co2=subdaily_data["co2"].to_numpy(),
+        patm=subdaily_data["patm"].to_numpy(),
+        fapar=np.ones_like(subdaily_data["tc"]),
+        ppfd=subdaily_data["ppfd"].to_numpy(),
     )
+
+    datetimes = subdaily_data["time"].to_numpy().astype("datetime64[ns]")
 
     # Set up the datetimes of the observations and set the acclimation window
     acclim = AcclimationModel(
-        datetimes=subdaily_data["time"].to_numpy(),
+        datetimes=datetimes,
         alpha=1 / 15,
     )
     acclim.set_window(
@@ -207,34 +182,24 @@ def test_faparlimitation_fromsubdailypmodel(site_data, splash_data, subdaily_dat
         method_kphio="temperature",
     )
 
-    # Reduce splash data down to the same datetimes as the other data
-    splash_data = splash_data.sel(
-        time=slice(subdaily_data["time"][0], subdaily_data["time"][-1])
-    )
-
-    aridity_index = splash_data["pet"].mean() / splash_data["pre"].mean()
+    aridity_index = site_data["AI_from_cruts"]
 
     # Find growing season
-    growing_season = subdaily_data["TA_F"] >= 0
+    growing_season = daily_to_subdaily(daily_data["growing_day"].to_numpy(), datetimes)
 
-    # Calculate soil moisture stress for gpp penalty
-    daily_soil_moisture_stress = calc_soilmstress_mengoli(
-        soilm=splash_data["wn"].to_numpy() / 150,
-        aridity_index=float(aridity_index),
-    )
-
+    # Get soil moisture stress for gpp penalty
     soil_moisture_stress = daily_to_subdaily(
-        daily_soil_moisture_stress, subdaily_data["time"].data
+        daily_data["PMod_gpp_smstress"].to_numpy(), datetimes
     )
 
     faparlim = FaparLimitation.from_subdailypmodel(
         subdaily_pmodel,
-        growing_season.data,
-        subdaily_data["time"].data,
-        subdaily_data["P_F_MOLAR"].data,
+        growing_season,
+        datetimes,
+        subdaily_data["precip_molar"],
         aridity_index,
         soil_moisture_stress,
     )
 
-    assert np.allclose(subdaily_data["fapar_max"].to_numpy(), faparlim.fapar_max)
-    assert np.allclose(subdaily_data["lai_max"].to_numpy(), faparlim.lai_max)
+    annual_lai_max = np.unique(daily_data["annual_lai_max"].to_numpy())
+    assert np.allclose(annual_lai_max, faparlim.lai_max)
