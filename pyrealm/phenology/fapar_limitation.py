c62564b8b42ee37edad37763ff8f4ab34faee9c8
David Orme <davidorme@users.noreply.github.com>
2025-07-17 14:45:56 +0100

    Fixing tests

--- pyrealm/phenology/fapar_limitation.py
+++ pyrealm/phenology/fapar_limitation.py
@@ -391,6 +391,8 @@ class FaparLimitation:
 class Phenology:
     """Phenology calculation."""
 
+    __experimental__ = True
+
     def __init__(
         self,
         daily_gpp: NDArray[np.floating],
@@ -399,6 +401,9 @@ class Phenology:
         alpha: float = 1 / 15,
         phenology_const: PhenologyConst = PhenologyConst(),
     ):
+        # Experimental class
+        warn_experimental(self.__class__.__name__)
+
         # Check the array input shapes
         check_input_shapes(daily_gpp, datetimes)
 
