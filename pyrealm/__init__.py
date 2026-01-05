4a081b05079bca1a46ab66d049b46ad89cad4eb0
David Orme <davidorme@users.noreply.github.com>
2026-01-05 13:31:35 +0000

    Remove overzealous warnings formatting

--- pyrealm/__init__.py
+++ pyrealm/__init__.py
@@ -4,23 +4,5 @@ TODO: complete this documentation
 """
 
 import importlib.metadata
-import os
-import warnings
 
 __version__ = importlib.metadata.version("pyrealm")
-
-
-# Setup warnings to simpler one line warning
-def warning_on_one_line(
-    message: Warning | str,
-    category: type[Warning],
-    filename: str,
-    lineno: int,
-    line: str | None = None,
-) -> str:
-    """Provide a simple one line warning for use in docstrings."""
-    filename = os.path.join("pyrealm", os.path.basename(filename))
-    return f"{filename}:{lineno}: {category.__name__}: {message}\n"
-
-
-warnings.formatwarning = warning_on_one_line
