f61ccffb3e0379000f64b1ff3334245534b4b199
David Orme <davidorme@users.noreply.github.com>
2026-07-09 14:43:05 +0100

    Updating rsplash golden datasets to include evap and solar

--- pyrealm_build_data/rsplash/__init__.py
+++ pyrealm_build_data/rsplash/__init__.py
@@ -7,6 +7,23 @@ sunshine fraction.
 
 The file ``rsplash_Bourne_example.R`` contains R code to install the ``rsplash`` package
 (https://github.com/dsval/rsplash/tree/master), run the documented Bourne example and
-save the predictions to the file ``rsplash_Bourne_outputs.csv`` . Only one of these
-(``netr``, net radiation) is used as a test case in ``pyrealm``.
-"""  # noqa: D205
+save the predictions to the file ``rsplash_Bourne_outputs.csv`` .
+
+The `rsplash` package does not have accessors to output the internals of the solar
+or evaporation calculations. The code in the example file was run using a modified
+build of `rsplash` that was simply patched to include a call to the EVAP.display()
+method within the loop in SPLASH.run_all() method:
+
+diff --git a/src/SPLASH.cpp b/src/SPLASH.cpp
+index 6c99c33..8c318cd 100644
+--- a/src/SPLASH.cpp
++++ b/src/SPLASH.cpp
+@@ -1906,6 +1906,8 @@ List SPLASH::run_all(vector<int> &doys, vector<int> &yrs, vector<double> &sw_in,
+         cond_vec[i] = dvap.cond;
+         netr_vec[i] = dvap.rn_d/1e6;
+         nds_vec[i] = dsoil.nd;  // # codespell:ignore
++
++        evap.display();
+     }
+
+"""  # noqa: D205, E501
--- pyrealm_build_data/rsplash/rsplash_Bourne_example.R
+++ pyrealm_build_data/rsplash/rsplash_Bourne_example.R
@@ -1,5 +1,3 @@
-
-
 # install.packages(
 #     "https://cran.r-project.org/src/contrib/Archive/topmodel/topmodel_0.7.5.tar.gz",
 #     repos = NULL, type = "source"
@@ -10,21 +8,30 @@
 
 
 library(rsplash)
+library(xts)
 
 data(Bourne)
+
+# Export forcings
+forcings <- Bourne$forcing
+forcings <- data.frame(date = index(forcings), coredata(forcings))
+forcings$elev <- Bourne$md$elev_m
+forcings$lat <- Bourne$md$lat
+write.csv(forcings, "rsplash_Bourne_inputs.csv")
+
 # run splash
-run1<-splash.point(
-    sw_in=Bourne$forcing$sw_in,  # shortwave radiation W/m2
-    tc=Bourne$forcing$Ta,        # air temperature C
-    pn= Bourne$forcing$P,        # precipitation mm
-    lat=Bourne$md$latitude,      # latitude deg
-    elev=Bourne$md$elev_m,       # elevation masl
-    slop=Bourne$md$slop_250m,    # slope deg
-    asp=Bourne$md$asp_250m,      # aspect deg
-    soil_data=Bourne$soil,       # soil data
-    Au=Bourne$md$Aups_250m,      # upslope area m2
-    resolution=250.0              resolution pixel dem used to get Au
+run1 <- splash.point(
+    sw_in = Bourne$forcing$sw_in, # shortwave radiation W/m2
+    tc = Bourne$forcing$Ta, # air temperature C
+    pn = Bourne$forcing$P, # precipitation mm
+    lat = Bourne$md$latitude, # latitude deg
+    elev = Bourne$md$elev_m, # elevation masl
+    slop = Bourne$md$slop_250m, # slope deg
+    asp = Bourne$md$asp_250m, # aspect deg
+    soil_data = Bourne$soil, # soil data
+    Au = Bourne$md$Aups_250m, # upslope area m2
+    resolution = 250.0 # resolution pixel dem used to get Au
 )
 
+# Export results
 write.csv(run1, "rsplash_Bourne_outputs.csv")
-
